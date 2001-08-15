8bc35091c17bc40f343e01e84b2213b87b8b3bf4
Robert Gaggl <robert@nomatic.org>
2001-08-15 09:04:30 +0000

    added isViewAllowed()-function, using isViewAllowed()-function

--- code/Site/renderFunctions.js
+++ code/Site/renderFunctions.js
@@ -1,29 +1,28 @@
 /**
  * function checks if a link to the given group makes sense
  */
- 
+
 function renderCalendarDay(currGroupname,text) {
    var now = new Date();
    var currGroup = this.get(currGroupname);
    if (currGroup && currGroup.size()) {
-      if (this.isUserAdmin() || this.isUserContributor())
+      if (this.isUserAdmin())
          var linkit = true;
       else {
          var linkit = false;
          if (currGroupname <= now.format("yyyyMMdd")) {
             for (var i=0;i<currGroup.size();i++) {
-               if (currGroup.get(i).isOnline())
+               if (currGroup.get(i).isOnline() || currGroup.get(i).isViewAllowed())
                   linkit = true;
             }
          }
       }
       if (linkit) {
-         var text = "<a href=\"" + this.href("main") + "?show=" + currGroupname + "\">" + text + "</a>";
+         var text = "<a href=\"" + currGroup.href() + "\">" + text + "</a>";
       }
    }
    return (text);
 }
-
 
 /**
  * function renders image-tag
