fce4aedd9a5ea7f87b50e61e57a5a210bdf0e5a8
Robert Gaggl <robert@nomatic.org>
2001-08-15 09:00:35 +0000

    initial check-in

---
+++ code/Day/macros.js
@@ -0,0 +1,13 @@
+/**
+ * macro rendering storylist for this day
+ * but check if story is online ...
+ * if not, we only display it when user is also allowed to edit this story!
+ */
+
+function storylist_macro() {
+   for (var i=0;i<this.size();i++) {
+      if (this.get(i).isOnline() || this.get(i).isViewAllowed())
+         this.get(i).renderSkin("preview");
+   }
+}
+
