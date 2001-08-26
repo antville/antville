dc878179f3675b41ed97ae62d77c5c3e7876d9b3
Robert Gaggl <robert@nomatic.org>
2001-08-26 16:18:59 +0000

    removed filter-function

--- code/Comment/filterFunctions.js
+++
@@ -1,8 +0,0 @@
-/**
- * function filters comments
- * only toplevel-comments should appear as subnodes of story
- */
-
-function filter() {
-   this.subnodeRelation = "WHERE PARENT_ID = " + this.__id__ + " ORDER BY CREATETIME asc";
-}
