143e790bc3278960ff0d17e0834d9de8adfc553f
Robert Gaggl <robert@nomatic.org>
2001-08-26 16:11:18 +0000

    removed filterfunction cause it's not needed anymore

--- code/Story/filterFunctions.js
+++
@@ -1,8 +0,0 @@
-/**
- * function filters comments
- * only toplevel-comments should appear as subnodes of story
- */
-
-function filter() {
-   this.subnodeRelation = "WHERE STORY_ID = " + this.__id__ + " AND PARENT_ID is null ORDER BY CREATETIME asc";
-}
