597b447d4199410c5d3a19859ba94d343b0df994
Robert Gaggl <robert@nomatic.org>
2001-09-05 21:17:11 +0000

    __id__ is now _id

--- code/Global/renderFunctions.js
+++ code/Global/renderFunctions.js
@@ -9,7 +9,7 @@
 function getActionName() {
    // return name of current action executed by user
    var rPath = req.path.split("/");
-   if (path[path.length-1].__id__ == rPath[rPath.length-1] || path[path.length-1].__name__ == rPath[rPath.length-1])
+   if (path[path.length-1]._id == rPath[rPath.length-1] || path[path.length-1]._name == rPath[rPath.length-1])
       return "main";
    else
       return(rPath[rPath.length -1]);
