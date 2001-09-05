71f3b7c8085a47bd44a5fe3181d1e7e7d25f7951
Robert Gaggl <robert@nomatic.org>
2001-09-05 21:22:55 +0000

    new functions: setLayout() and formatLinks()

--- code/Global/objectFunctions.js
+++ code/Global/objectFunctions.js
@@ -58,3 +58,54 @@ function isClean(str) {
       return false;
    return true;
 }
+
+/**
+ * function checks if there is a weblog-object in path
+ * if true, it sets the skin of response to "page" and returns the weblog-object found
+ * if false, it uses root and it's page-skin
+ */
+
+function setLayout() {
+   if (path.weblog) {
+      res.skin = "weblog.page";
+      return (path.weblog);
+   } else {
+      res.skin = "root.page";
+      return (root);
+   }
+}
+
+/**
+ * function creates macro-tags out of plain urls in text
+ * does this with three replace-operations
+ */
+
+function formatLinks(str) {
+   var pre = "<% this.link to=\"";
+   var mid = "\" text=\"";
+   var post = "\" %>";
+   var l0 = new RegExp("<a href\\s*=\\s*\"?([^\\s\"]+)?\"?[^>]*?>([^<]*?)</a>");
+   var l1 = new RegExp("([fhtpsr]+:\\/\\/[^\\s]+?)([\\.,;\\)\\]\"]?(\\s|$))");
+   var l2 = new RegExp("(<%[^%]*?)" + pre + "(.*?)" + mid + ".*?" + post + "([^%]*?%>)");
+   l0.ignoreCase = l1.ignoreCase = l2.ignoreCase = true;
+   l0.global = l1.global = l2.global = true;
+   
+   str = str.replace(l0, pre + "$1" + mid + "$2" + post);
+   str = str.replace(l1, pre + "$1" + mid + "$1" + post + "$3");
+   str = str.replace(l2, "$1$2$3");
+   /* old version:
+   var pre = "<% this.link to=\"";
+   var mid = "\" text=\"";
+   var post = "\" %>";
+   var l0 = new RegExp("<\\s*a\\s*href\\s*=\\s*\"?([^\\s\"]+)?\"?[^>]*?>([^<]*?)</a>");
+   var l1 = new RegExp("([fhtpsr]+:\\/\\/[^\\s]+?)([\\.,;\\)\\]\"]?(\\s|$))");
+   var l2 = new RegExp("(<%[^%]*?)" + pre + "(.*?)" + mid + ".*?" + post + "([^%]*?%>)");
+   l0.ignoreCase = l1.ignoreCase = l2.ignoreCase = true;
+   l0.global = l1.global = l2.global = true;
+   
+   str = str.replace(l0, pre + "$1" + mid + "$2" + post);
+   str = str.replace(l1, pre + "$1" + mid + "link" + post + "$2");
+   str = str.replace(l2, "$1$2$3");
+   */
+   return (str);
+}
