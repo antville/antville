9dfed4eaa0693da84c1caa0fd2821ffca9fa152f
Robert Gaggl <robert@nomatic.org>
2001-10-02 10:57:12 +0000

    added fetchSkin(), deleteSkin(), modified saveSkin()

--- code/SkinMgr/objectFunctions.js
+++ code/SkinMgr/objectFunctions.js
@@ -1,4 +1,21 @@
 /**
+ * function tries to get the skin-object from skinmanager
+ * if not existing, returns a temporary skin-object
+ * containing the source of the file-skin
+ */
+
+function fetchSkin(proto,name) {
+   var currProto = this._parent.skinmanager.get(proto);
+   if (currProto && currProto.get(name))
+      return(currProto.get(name));
+   else {
+      var s = new skin();
+      s.skin = app.skinfiles[proto][name];
+      return (s);
+   }
+}
+
+/**
  * function stores skin
  * if no source was posted, the database-skin is deleted
  * (actually there should be a "delete"-button ...)
@@ -6,23 +23,28 @@
 
 function saveSkin() {
    if (req.data.proto && req.data.name) {
-      var currProto = this._parent.skinmanager.get(req.data.proto);
-      if (currProto) {
-         var currSkin = currProto.get(req.data.name);
-      }
-      if (!currSkin && req.data.skin) {
-         var currSkin = new skin();
-         currSkin.creator = user;
-         currSkin.createtime = new Date();
-         currSkin.name = req.data.name;
-         currSkin.proto = req.data.proto;
-         this._parent.skinmanager.add(currSkin);
-      } else if (!req.data.skin) {
-         currProto.remove(currSkin);
-      }
+      var s = this.fetchSkin(req.data.proto,req.data.name);
+      if (!s.proto && req.data.skin) {
+         s.creator = user;
+         s.createtime = new Date();
+         s.name = req.data.name;
+         s.proto = req.data.proto;
+         this._parent.skinmanager.add(s);
+      } else if (s.proto && !req.data.skin)
+         this._parent.skinmanager.get(s.proto).remove(s);
       if (req.data.skin)
-         currSkin.skin = req.data.skin;
+         s.skin = req.data.skin;
       res.message = "Changes were saved successfully!";
    }
+   res.redirect(this.href() + "#" + s.proto + s.name);
+}
+
+/**
+ * function deletes a skin
+ */
+
+function deleteSkin(s) {
+   this._parent.skinmanager.get(s.proto).remove(s);
+   res.message = "Skin deleted successfully!";
    res.redirect(this.href());
 }
