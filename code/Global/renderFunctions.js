78d298804b1b49eaaf15661ab71768987aaeb94d
Hannes Wallnöfer <hannesw@gmail.com>
2001-12-04 13:14:09 +0000

    added doWikiStuff() function to do wiki style substitution on a given piece of text.
    Also added simpleDropDownBox, which takes an array
    as parameter instead of the drop down box framework in
    hopobject, which requires a complex object structure.

--- code/Global/renderFunctions.js
+++ code/Global/renderFunctions.js
@@ -55,3 +55,62 @@ function renderTextPreview(text,limit) {
    for (var i=0;i<prev.length;i=i+30)
       res.write(prev.substring(i,i+30) + "<wbr>");
 }
+
+/**
+ * Do Wiki style substitution, transforming
+ * stuff contained between asterisks into links.
+ */
+function doWikiStuff (src) {
+   var src= " "+src;
+
+   // set up reg exp for http link stuff
+   var linkexp = new RegExp ("([^=^\"])(http://[^ ^\r^\n^\"]*)");
+   linkexp.ignoreCase=true;
+   linkexp.global=true;
+
+   // do the Wiki link thing, *asterisk style*
+   var regex = new RegExp ("[*]([a-z0-9צהü][a-z0-9צהüß ]*[a-z0-9צהüß])[*]");
+   regex.ignoreCase=true;
+
+   var text = "";
+   var start = 0;
+   while (true) {
+      var found = regex.exec (src.substring(start));
+      var to = found == null ? src.length : start + found.index;
+      text += src.substring(start, to).replace (linkexp, "$1<a href=\"$2\">$2</a>");
+      if (found == null)
+         break;
+      var name = found[1];
+      var item = path.weblog.space.get (name);
+      if (item == null && name.lastIndexOf("s") == name.length-1)
+         item = path.weblog.space.get (name.substring(0, name.length-1));
+      if (item == null)
+         text += format(name)+" <small>[<a href=\""+path.weblog.stories.href("create")+"?topic="+escape(name)+"\">define "+format(name)+"</a>]</small>";
+      else
+         text += "<a href=\""+item.href()+"\">"+name+"</a>";
+      start += found.index + found[1].length+2;
+   }
+   return text;
+}
+
+/**
+ *  Renders a drop down box from an Array and an optional 
+ *  current selection index. This is a simpler alternative 
+ *  for the drop-down framework in hopobject. Its main 
+ *  advantage is that Arrays are much simpler to set up in 
+ *  JavaScript than (Hop)Objects:
+ *  
+ */
+function simpleDropDownBox (name, options, selectedIndex) {
+  var str = "<select name=\""+name+"\" size=\"1\">";
+  for (var i in options) {
+    var name = encode (options[i]);
+    var key = i;
+    if (key == selectedIndex)
+       str += "<option value=\""+key+"\" selected=\"true\">"+name+"</option>";
+    else
+       str += "<option value=\""+key+"\">"+name+"</option>";
+  }
+  str += "</select>";
+  return str;
+}
