41f273a06e6146280a48210f60dde9318f65ecca
Hannes Wallnöfer <hannesw@gmail.com>
2001-12-04 13:31:16 +0000

    initial check-in of topic prototype. This inherits from the day prototype. Similar to day, it is a groupby prototype which contains story object. Additionally to the day prototype, it has macros to add stories to the topic, and a macro to display related topics (i.e. topics or days which contain stories that link to this topic).

---
+++ code/Topic/macros.js
@@ -0,0 +1,31 @@
+/**
+ * Display a link to let the use add a new writeup 
+ * to this topic.
+ */
+function addstory_macro () {
+   if (path.weblog.isUserContributor()|| path.weblog.isUserAdmin()) {
+      var param = new Object();
+      param.link = path.weblog.stories.href("create")+"?topic="+this.groupname;
+      this.renderSkin ("createStoryLink", param);
+   }
+}
+
+/**
+ * Get related topics, i.e. topics that contain stories that
+ * link back to this topic.
+ */
+function relatedtopics_macro (param) {
+   var duplicateChecker = new HopObject();
+   this.related.subnodeRelation = "where TEXT like '%"+this.groupname+"%' ";
+   var l = this.related.size();
+   if (l == 0)
+      return;
+   res.write (param.prefix);
+   for (var i=0; i<l; i++) {
+      var s = this.related.get(i);
+      s._parent.renderSkin("relatedLink");
+   }
+   res.write (param.suffix);
+}
+
+
