1cdedc54447e3f12b78c17fb6a0ca5165f72c6d8
Robert Gaggl <robert@nomatic.org>
2001-12-10 23:33:09 +0000

    - functions now have a defined (and documented ...) interface - added comments to functions

--- code/StoryMgr/objectFunctions.js
+++ code/StoryMgr/objectFunctions.js
@@ -1,55 +1,74 @@
 /**
  * function checks if story fits to the minimal needs (must have at least a text ;-)
+ * @param Obj Object containing the properties needed for creating a new story
+ * @param Obj User-Object creating this story
+ * @return Obj Object containing three properties:
+ *             - error (boolean): true if error happened, false if everything went fine
+ *             - message (String): containing a message to user
+ *             - id (Int): id of created story
  */
 
-function evalNewStory() {
-   var newStory = new story();
-   
-   if (req.data.text) {
-      newStory.weblog = path.weblog;
-      newStory.title = req.data.title;
-      newStory.text = req.data.text;
-      newStory.online = parseInt(req.data.online) ? parseInt(req.data.online) : 0;
-      if (req.data.editableby != null && req.data.editableby != "")
-         newStory.editableby = parseInt(req.data.editableby,10);
-      else
-         newStory.editableby = 2;
-      newStory.author = user;
+function evalNewStory(param,creator) {
+   var result = new Object();
+   if (param.text && creator) {
+      result.url = this.href();
+      var newStory = new story();
+      var online = parseInt(param.online,10);
+      var editableby = parseInt(param.editableby,10);
+      newStory.weblog = this._parent;
+      newStory.title = param.title;
+      newStory.text = param.text;
+      newStory.online = !isNaN(online) ? online : 0;
+      newStory.editableby = !isNaN(editableby) ? editableby : 2;
+      newStory.author = creator;
       newStory.createtime = new Date();
       newStory.modifytime = new Date();
-      if (req.data.topic)
-         newStory.topic = req.data.topic;
+      if (param.topic)
+         newStory.topic = param.topic;
       newStory.day = newStory.createtime.format("yyyyMMdd");
       newStory.reads = 0;
-      path.weblog.add(newStory);
-      if (newStory.online)
-         path.weblog.lastupdate = newStory.createtime;
-      res.message = "The story was created successfully!";
-      res.redirect(this.href());
+      this._parent.add(newStory);
+      if (newStory.online) {
+         this._parent.lastupdate = newStory.createtime;
+         if (newStory.topic)
+            result.url = this._parent.space.href() + newStory.topic;
+         else
+            result.url = newStory.href();
+      } else
+         result.url = this.href();
+      result.message = "The story was created successfully!";
+      result.id = newStory._id;
+      result.error = false;
    } else {
-      // set wiki topic, if defined
-      if (req.data.topic)
-         newStory.topic = req.data.topic;
-      return (newStory);
+      result.message = "Please fill out the form to create a new story";
+      result.error = true;
    }
+   return (result);
 }
 
 
 /**
  * delete a story
  * including all the comments
+ * @param Obj Story-Object that should be deleted
+ * @return Obj Object containing two properties:
+ *             - error (boolean): true if error happened, false if everything went fine
+ *             - message (String): containing a message to user
  */
 
 function deleteStory(currStory) {
+   var result = new Object();
    for (var i=currStory.size();i>0;i--)
       currStory.deleteComment(currStory.get(i-1));
-   currStory.setParent(path.weblog);
-   if (path.weblog.remove(currStory)) {
-      path.weblog.lastupdate = new Date();
-      res.message = "The story was deleted successfully!";
-   } else
-      res.message = "Ooops! Couldn't delete the story!";
-
-   res.redirect(this.href());
+   currStory.setParent(this._parent);
+   if (this._parent.remove(currStory)) {
+      this._parent.lastupdate = new Date();
+      result.message = "The story was deleted successfully!";
+      result.error = false;
+   } else {
+      result.message = "Ooops! Couldn't delete the story!";
+      result.error = true;
+   }
+   return (result);
 }
 
