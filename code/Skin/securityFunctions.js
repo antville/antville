f88cdf8734656dd57953dcb121ff91884e98b9b7
Robert Gaggl <robert@nomatic.org>
2001-12-10 23:16:58 +0000

    - added comments to functions - security-functions now demand user-object as argument

--- code/Skin/securityFunctions.js
+++ code/Skin/securityFunctions.js
@@ -1,11 +1,13 @@
 /**
  * check if user is allowed to delete this skin
+ * @param Obj Userobject
+ * @return String Reason for denial (or null if allowed)
  */
 
-function isDeleteDenied() {
-   if (user.isBlocked())
+function isDeleteDenied(usr) {
+   if (usr.isBlocked())
       return ("Sorry, your account was disabled!");
-   else if (!this.weblog.isUserAdmin())
+   else if (!this.weblog.isUserAdmin(usr))
       return ("Only admins of this weblog can delete skins!");
    return null;
 }
