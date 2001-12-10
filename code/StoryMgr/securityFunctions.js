e3d6edf3ff4e603cd025fbaaff8e2aa807bab3d3
Robert Gaggl <robert@nomatic.org>
2001-12-10 23:31:25 +0000

    - security-functions now demand user-object as argument

--- code/StoryMgr/securityFunctions.js
+++ code/StoryMgr/securityFunctions.js
@@ -1,30 +1,34 @@
 /**
  * function checks if user is allowed to view the storylist
  * of this weblog
+ * @param Obj Userobject
+ * @return String Reason for denial (or null if allowed)
  */
 
-function isDenied() {
-   if (!user.uid) {
-      user.cache.referer = this.href();
+function isDenied(usr) {
+   if (!usr.uid) {
+      usr.cache.referer = this.href();
       return ("Please login before!");
-   } else if (user.isBlocked())
+   } else if (usr.isBlocked())
       return ("Sorry, your account was disabled!");
-   else if (!this._parent.isUserAdmin() && !this._parent.isUserContributor())
+   else if (!this._parent.isUserAdmin(usr) && !this._parent.isUserContributor(usr))
       return ("You're not allowed to edit stories!");
    return null;
 }
 
 /**
  * check if user is allowed to add a story to this weblog
+ * @param Obj Userobject
+ * @return String Reason for denial (or null if allowed)
  */
 
-function isAddDenied() {
-   if (!user.uid) {
-      user.cache.referer = this.href("create");
+function isAddDenied(usr) {
+   if (!usr.uid) {
+      usr.cache.referer = this.href("create");
       return ("Please login before adding a story!");
-   } else if (user.isBlocked())
+   } else if (usr.isBlocked())
       return ("Sorry, your account was disabled!");
-   else if (!path.weblog.isUserAdmin() && !path.weblog.isUserContributor() && !path.weblog.userMayContrib())
+   else if (!path.weblog.isUserAdmin(usr) && !path.weblog.isUserContributor(usr) && !path.weblog.userMayContrib())
       return ("You're not allowed to add a story to a foreign weblog!");
    return null;
 }
