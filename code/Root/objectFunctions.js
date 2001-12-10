39540adfd7df9b8dd94b733f2e1cbeb90149cc5b
Robert Gaggl <robert@nomatic.org>
2001-12-10 23:14:00 +0000

    - functions now have a defined (and documented ...) interface

--- code/Root/objectFunctions.js
+++ code/Root/objectFunctions.js
@@ -2,54 +2,56 @@
  * evaluating new weblog
  */
 
-function evalNewWeblog() {
-   var newLog = new weblog();
-   if (req.data.title && req.data.alias) {
-      newLog.title = req.data.title;
-      newLog.alias = req.data.alias.toLowerCase();
-      if (this.checkIfExists(newLog.alias)) {
-         res.message = "Sorry, we already have a weblog with this alias!";
-         newLog.error = true;
-      } else if (!isClean(newLog.alias)) {
-         res.message = "Please don't use any special characters in the alias!";
-         newLog.error = true;
-      } else {
-         // now we can safely create a new weblog
-         this.createNewWeblog(newLog);
-      }
-   } else {
-      newLog.error = true;
-      newLog.title = newLog.alias = "";
-      if (!res.message) res.message = "Please fill in the form to create your new Weblog!";
+function evalWeblog(title,alias,creator) {
+   var result = new Object();
+   if (!alias) {
+      result.message = "Please choose an alias for your weblog!";
+      result.error = true;
+   } else if (this.get(alias)) {
+      result.message = "Sorry, we already have a weblog with this alias!";
+      result.error = true;
+   } else if (!isClean(alias)) {
+      result.message = "Please don't use any special characters in the alias!";
+      result.error = true;
    }
-   return (newLog);
+   if (!title) {
+      result.message = "Please choose a title for your weblog!";
+      result.error = true;
+   }
+   // check if alias is similar to an action-name (which is reserved)
+   var reserved = eval("this." + alias + "_action");
+   if (reserved) {
+      result.message = "Sorry, this alias is reserved!";
+      result.error = true;
+   }
+   if (!result.error) {
+      result.weblog = this.createNewWeblog(title,alias,creator);
+      if (!result.weblog) {
+         result.message = "Couldn't create your weblog!";
+         result.error = true;
+      } else
+         result.message = "Your weblog was created successfully!";
+   }
+   return (result);
 }
 
-
-/**
- * check if alias is already in use
- */
-
-function checkIfExists(alias) {
-   if (this.get(alias))
-      return true;
-   return false;
-}
 
 /**
  * create a new weblog
  */
 
-function createNewWeblog(newLog) {
-   newLog.creator = user;
-   newLog.createtime = new Date();
+function createNewWeblog(title,alias,creator) {
+   var newLog = new weblog();
+   newLog.title = title;
+   newLog.alias = alias;
+   newLog.creator = creator;
+   newLog.createtime = newLog.birthdate = new Date();
    newLog.online = 0;
    newLog.discussions = 1;
    newLog.usercontrib = 0;
    newLog.usersignup = 1;
    newLog.archive = 1;
    newLog.blocked = 0;
-   newLog.birthdate = new Date();
    newLog.bgcolor = "FFFFFF";
    newLog.textfont = "Verdana, Arial, Helvetica, sans-serif";
    newLog.textsize = "9pt";
@@ -60,29 +62,19 @@ function createNewWeblog(newLog) {
    newLog.titlefont = "Verdana, Arial, Helvetica, sans-serif";
    newLog.titlesize = "11pt";
    newLog.titlecolor = "CC0000";
+   newLog.smallfont = "Arial, Helvetica, sans-serif";
+   newLog.smallsize = "8pt";
+   newLog.smallcolor = "999999";
    newLog.days = 3;
    newLog.language = "en";
    newLog.country = "US";
-   newLog.dateformat = "EEEE, dd.MM.yyyy HH:mm";
+   newLog.longdateformat = "EEEE, dd. MMMM yyyy, h:mm a";
+   newLog.shortdateformat = "yyyy.MM.dd, HH:mm";
    newLog.createImgDirectory()
-   this.add(newLog);
-   // create member-object for connecting user <-> weblog with admin-rights
-   newLog.createMember(2);
-   res.message = "Your weblog was created successfully! Have fun!";
-   res.redirect(newLog.href());
+   if (this.add(newLog)) {
+      // create member-object for connecting user <-> weblog with admin-rights
+      newLog.createMember(creator,2);
+      return (newLog);
+   } else
+      return null;
 }
-
-/**
- * alias of image has changed, so we remove it and add it again with it's new alias
- */
-
-function changeAlias(currImg) {
-   // var oldAlias = currImg.alias;
-   currImg.setParent(this);
-   this.remove(currImg);
-   this.set(currImg.alias,null);
-   currImg.alias = req.data.alias;
-   this.add(currImg);
-   return;
-}
-
