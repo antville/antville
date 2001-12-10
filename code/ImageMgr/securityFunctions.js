4ba598067443fdba13c2b62ae45decb7b06b8425
Robert Gaggl <robert@nomatic.org>
2001-12-10 23:03:22 +0000

    - security-functions now demand user-object as argument - added comments to functions

--- code/ImageMgr/securityFunctions.js
+++ code/ImageMgr/securityFunctions.js
@@ -1,29 +1,33 @@
 /**
  * check if user is allowed to edit images
+ * @param Obj Userobject
+ * @return String Reason for denial (or null if allowed)
  */
 
-function isEditDenied() {
-   if (!user.uid) {
-      user.cache.referer = this.href();
+function isEditDenied(usr) {
+   if (!usr.uid) {
+      usr.cache.referer = this.href();
       return ("Please login before");
-   } else if (user.isBlocked())
+   } else if (usr.isBlocked())
       return ("Sorry, your account was disabled!");
-   else if (!this._parent.isUserAdmin() && !this._parent.isUserContributor() && !this._parent.userMayContrib())
+   else if (!this._parent.isUserAdmin(usr) && !this._parent.isUserContributor(usr) && !this._parent.userMayContrib())
       return ("Sorry, you're not allowed to edit images!");
    return null;  
 }
 
 /**
  * check if user is allowed to add images
+ * @param Obj Userobject
+ * @return String Reason for denial (or null if allowed)
  */
 
-function isAddDenied() {
-   if (!user.uid) {
-      user.cache.referer = this.href("create");
+function isAddDenied(usr) {
+   if (!usr.uid) {
+      usr.cache.referer = this.href("create");
       return ("Please login before");
-   } else if (user.isBlocked())
+   } else if (usr.isBlocked())
       return ("Sorry, your account was disabled!");
-   else if (!this._parent.isUserAdmin() && !this._parent.isUserContributor() && !this._parent.userMayContrib())
+   else if (!this._parent.isUserAdmin(usr) && !this._parent.isUserContributor(usr) && !this._parent.userMayContrib())
       return ("Sorry, you're not allowed to add images!");
    return null;  
 }
