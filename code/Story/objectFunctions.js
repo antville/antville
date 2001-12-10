a22c89048fb28adb0347ce8e5e7ff10f823b5be9
Robert Gaggl <robert@nomatic.org>
2001-12-10 23:24:12 +0000

    - new function getText() that caches preprocessed text of story - functions now have a defined (and documented ...) interface - added comments to functions

--- code/Story/objectFunctions.js
+++ code/Story/objectFunctions.js
@@ -1,37 +1,43 @@
 /**
  * check if story is ok; if true, save changed story
+ * @param Obj Object containing the properties needed for creating a new story
+ * @param Obj User-Object modifying this story
+ * @return Obj Object containing two properties:
+ *             - error (boolean): true if error happened, false if everything went fine
+ *             - message (String): containing a message to user
  */
 
-function evalStory() {
-   if (req.data.text) {
-      if (user == this.author && req.data.editableby != null && req.data.editableby != "")
-         this.editableby = parseInt(req.data.editableby,10);
-      else
-         this.editableby = 2;
-      this.title = req.data.title;
-      this.text = req.data.text;
-      this.online = parseInt(req.data.online,10);
+function evalStory(param,modifier) {
+   var result = new Object();
+   if (param.text) {
+      var online = parseInt(param.online,10);
+      var editableby = parseInt(param.editableby,10);
+      if (this.online)
+         this.weblog.lastupdate = new Date();
+      this.online = (!isNaN(online) ? online : 0);
+      this.editableby = (modifier == this.author && !isNaN(editableby) ? editableby : 2);
+      this.title = param.title;
+      this.text = param.text;
       this.modifytime = new Date();
-      this.modifier = user;
-      if (req.data.topic)
-         this.topic = req.data.topic;
-      else
-         this.topic = null;
+      this.modifier = modifier;
+      this.topic = param.topic ? param.topic : null;
       this.weblog.lastupdate = new Date();
-      res.message = "The story was updated successfully!";
-      if (user.cache.referer) {
-         var redirTo = user.cache.referer;
-         user.cache.referer = null;
-      }
-      // href() may not yet work if we changed the topic
-      // so we build the redirect URL manually
-      if (this.topic)
-         res.redirect(path.weblog.space.href()+this.topic+"/"+this._id);
-      else
-         res.redirect(path.weblog.href()+this.day+"/"+this._id);
-      // res.redirect(redirTo ? redirTo : this.weblog.stories.href());
-   } else
-      res.message = "You need at least some text!";
+      if (this.online) {
+         // href() may not yet work if we changed the topic
+         // so we build the redirect URL manually
+         if (this.topic)
+            result.url = this.weblog.space.href() + this.topic + "/" + this._id;
+         else
+            result.url = this.href();
+      } else
+         result.url = this.weblog.stories.href();
+      result.message = "The story was updated successfully!";
+      result.error = false;
+   } else {
+      result.message = "You need at least some text!";
+      result.error = true;
+   }
+   return (result);
 }
 
 /**
@@ -44,7 +50,7 @@ function toggleOnline(newStatus) {
       this.weblog.lastupdate = new Date();
    } else if (newStatus == "offline")
       this.online = 0;
-   res.redirect(this.weblog.stories.href());
+   return true;
 }
 
 /**
@@ -61,32 +67,58 @@ function isOnline() {
  * function evaluates comment and adds it if ok
  */
 
-function addComment() {
-   if (user.uid && !user.isBlocked() && (req.data.submit != "cancel" &&  !req.data.cancel)) {
+function evalComment(param,creator) {
+   var result = new Object();
+   if (!param.text) {
+      result.message = "You need at least some text!";
+      result.error = true;
+   } else {
       var c = new comment();
-      c.title = req.data.title;
-      c.text = req.data.text;
+      c.title = param.title;
+      c.text = param.text;
       c.weblog = this.weblog;
       c.story = this;
-      c.createtime = new Date();
-      c.modifytime = new Date();
-      c.author = user;
+      c.createtime = c.modifytime = new Date();
+      c.author = creator;
       c.online = 1;
-      c.ipadress = req.data.http_remotehost;
+      c.ipadress = param.http_remotehost;
       this.add(c);
       this.weblog.lastupdate = new Date();
+      result.message = "Your posting was saved successfully!";
+      result.error = false;
    }
-   res.redirect(this.href());
+   return (result);
 }
 
 /**
  * function deletes a whole thread
+ * @param Obj Comment-Object that should be deleted
+ * @return String Message indicating success/failure
  */
 
 function deleteComment(currComment) {
    for (var i=currComment.size();i>0;i--)
       currComment.deleteComment(currComment.get(i-1));
-   currComment.setParent(this);
-   this.remove(currComment);
-   res.message = "The comment was deleted successfully!";
+   if (this.remove(currComment))
+      return("The comment was deleted successfully!");
+   else
+      return("Couldn't delete the comment!");
+}
+
+/**
+ * function checks if the text of the story was already cached
+ * and if it's still valid
+ * if false, it caches it again
+ * @return String cached text of story
+ */
+
+function getText() {
+   if (this.cache.lrText <= this.modifytime) {
+      // cached version of text is too old, so we cache it again
+      var s = createSkin(format(activateLinks(this.text)));
+      this.allowTextMacros(s);
+      this.cache.lrText = new Date();
+      this.cache.rText = this.renderSkinAsString(s);
+   }
+   return (doWikiStuff(this.cache.rText));
 }
