393b3e470b22bf53f1c57029a01b96703513baf1
Robert Gaggl <robert@nomatic.org>
2001-12-10 23:43:11 +0000

    - security-functions now demand user-object as argument

--- code/Site/securityFunctions.js
+++ code/Site/securityFunctions.js
@@ -1,93 +1,106 @@
 /**
  * check if weblog is online
- * return false if not
+ * @param Obj Userobject
+ * @return String String indicating that weblog is not public (or null if public)
  */
 
-function isNotPublic() {
-   if (!this.isOnline() && !this.isUserMember())
+function isNotPublic(usr) {
+   if (!this.isOnline() && !this.isUserMember(usr))
       return("This weblog is not public!");
    return null;
 }
 
 /**
  * check if user is allowed to edit the preferences of this weblog
+ * @param Obj Userobject
+ * @return String Reason for denial (or null if allowed)
  */
 
-function isEditDenied() {
-   if (!user.uid) {
-      user.cache.referer = this.href("edit");
+function isEditDenied(usr) {
+   if (!usr.uid) {
+      usr.cache.referer = this.href("edit");
       return ("Please login to edit the preferences of this weblog!");
-   } else if (user.isBlocked())
+   } else if (usr.isBlocked())
       return ("Sorry, your account was disabled!");
-   else if (!this.isUserAdmin())
+   else if (!this.isUserAdmin(usr))
       return ("You're not allowed to edit the preferences!");
    return null;
 }
 
 /**
  * check if user is allowed to edit the memberlist of this weblog
+ * @param Obj Userobject
+ * @return String Reason for denial (or null if allowed)
  */
 
-function isEditMembersDenied() {
-   if (!user.uid) {
-      user.cache.referer = this.href("memberships");
+function isEditMembersDenied(usr) {
+   if (!usr.uid) {
+      usr.cache.referer = this.href("memberships");
       return ("Please login before!");
-   } else if (user.isBlocked())
+   } else if (usr.isBlocked())
       return ("Sorry, your account was disabled!");
-   else if (!this.isUserAdmin())
+   else if (!this.isUserAdmin(usr))
       return ("You're not allowed to edit memberships!");
    return null;
 }
 
 /**
  * function checks if the user is admin of this weblog
+ * @param Obj Userobject
+ * @return Boolean true if user is admin, false if not
  */
 
-function isUserAdmin() {
-   if (!this.isUserMember())
+function isUserAdmin(usr) {
+   if (!this.isUserMember(usr))
       return false;
-   else if (!this.members.get(user.name).isAdmin())
+   else if (!this.members.get(usr.name).isAdmin())
       return false;
    return true;
 }
 
 /**
  * function checks if user is a member of this weblog
+ * @param Obj Userobject
+ * @return Boolean true if user is contributor, false if not
  */
 
-function isUserContributor() {
-   if (user.uid && this.userMayContrib())
+function isUserContributor(usr) {
+   if (usr.uid && this.userMayContrib())
       return true;
-   else if (this.isUserMember() && this.members.get(user.name).isContributor())
+   else if (this.isUserMember(usr) && this.members.get(usr.name).isContributor())
       return true;
    return false;
 }
 
 /**
  * function checks if user is a member of this weblog
+ * @param Obj Userobject
+ * @return Boolean true if user is member, false if not
  */
 
-function isUserMember() {
-   if (!user.uid || !this.members.get(user.name))
+function isUserMember(usr) {
+   if (!usr.uid || !this.members.get(usr.name))
       return false;
    return true;
 }
 
 /**
  * function checks if user is allowed to sign up
+ * @param Obj Userobject
+ * @return String Reason for denial (or null if allowed)
  */
 
-function isSignUpDenied() {
-   if (this.isUserMember())
+function isSignUpDenied(usr) {
+   if (this.isUserMember(usr))
       return ("You are already a member of this weblog!");
    else if (!this.isOnline())
       return ("This weblog is not public!");
    else if (!this.userMaySignup())
       return ("Signing up was disabled!");
-   else if (!user.uid) {
-      user.cache.referer = this.href("signup");
+   else if (!usr.uid) {
+      usr.cache.referer = this.href("signup");
       return ("Please login before!");
-   } else if (user.isBlocked())
+   } else if (usr.isBlocked())
       return ("Sorry, your account was disabled!");
    return null;
 }
@@ -96,10 +109,11 @@ function isSignUpDenied() {
 /**
  * function checks if normal users are allowed to
  * contrib to this weblog
+ * @return Boolean true if members may contribute, false if not
  */
 
 function userMayContrib() {
-   if (parseInt(this.usercontrib))
+   if (parseInt(this.usercontrib,10))
       return true;
    return false;
 }
@@ -107,6 +121,7 @@ function userMayContrib() {
 
 /**
  * function checks if archive of weblog is enabled
+ * @return Boolean true if archive is enabled, false if not
  */
 
 function showArchive() {
