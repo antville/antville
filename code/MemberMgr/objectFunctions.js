312796ab52006cb4874f9060a399b62d7e6bbb50
Robert Gaggl <robert@nomatic.org>
2001-12-10 23:11:01 +0000

    - added comments to functions - functions now have a defined (and documented ...) interface

--- code/MemberMgr/objectFunctions.js
+++ code/MemberMgr/objectFunctions.js
@@ -1,14 +1,21 @@
 /**
  * check if a login attempt is ok
+ * @param String username
+ * @param String password
+ * @return Obj Object containing two properties:
+ *             - error (boolean): true if error happened, false if everything went fine
+ *             - message (String): containing a message to user
  */
 
-function evalLogin() {
+function evalLogin(username,password) {
+   var result = new Object();
+   result.error = true;
+
    // check if login is successful
-   if (user.login(req.data.name, req.data.password)) {
+   if (user.login(username, password)) {
       if (user.isBlocked()) {
-         res.message = "Sorry, your account was disabled!";
+         result.message = "Sorry, your account was disabled!";
          user.logout();
-         res.redirect(parent.href());
       }
       // login successful
       user.lastVisit = new Date();
@@ -17,155 +24,147 @@ function evalLogin() {
          res.setCookie("avUsr",user.name,365);
          res.setCookie("avPw",calcMD5(user.password),365);
       }
-      res.message = "Welcome to Antville, " + user.name + "! Have fun!";
-      if (!user.cache.referer) {
-         var redirectTo = root.href();
-      } else {
-         var redirectTo = user.cache.referer;
-         user.cache.referer = null;
-      }
-      res.redirect(redirectTo);
+      result.message = "Welcome to Antville, " + user.name + "! Have fun!";
+      result.error = false;
    } else
-      res.message = "Login failed! Maybe a typo?";
+      result.message = "Login failed! Maybe a typo?";
+   return (result);
 }
 
 /**
  * check if a registration attempt is ok
+ * @param Obj Object containing form-values needed for registration
+ * @return Obj Object containing four properties:
+ *             - error (boolean): true if error happened, false if everything went fine
+ *             - message (String): containing a message to user
+ *             - username: username of registered user
+ *             - password: password of registered user
  */
 
-function evalRegistration() {
-   var reg = this.checkReg();
-   if (!reg.error) {
-		var newUser = user.register(reg.name, reg.password1);
+function evalRegistration(param) {
+   var result = new Object();
+   result.error = true;
+
+   // check if email-address is valid
+   if (!param.email)
+      result.message = "Please type in your email-address!";
+   else if (!checkEmail(param.email))
+      result.message = "Ooops! The email-address you entered is not valid!";
+
+   // check if passwords match
+	if (!param.password1 || !param.password2)
+	   result.message = "Please type in your password twice!";
+	else if (param.password1 != param.password2)
+      result.message = "Passwords didn't match! Please re-enter:";
+
+   // check if username is existing and is clean
+   if (!param.name)
+      result.message = "Please choose a username!";
+   else if (!isClean(param.name))
+      result.message = "Please don't use any special characters in your username!";
+
+   if (!result.message) {
+		var newUser = user.register(param.name, param.password1);
  		if (newUser) {
-         newUser.name = reg.name;
-			newUser.email = reg.email;
-         newUser.url = reg.url;
-         newUser.description = reg.description;
+         newUser.name = param.name;
+			newUser.email = param.email;
+         newUser.url = evalURL(param.url);
+         newUser.description = param.description;
          newUser.registered = new Date();
          newUser.blocked = 0;
-			user.login(reg.name, reg.password1);
-         user.sendConfirmationMail();
-			res.message = "Welcome " + user.name + ". Have fun!<br>";
-			if (user.cache.referer) {
-			   var redirectTo = user.cache.referer;
-			   user.cache.referer = null;
-			} else
-			   var redirectTo = root.href();
-			res.redirect(redirectTo);
+			result.message = "Welcome " + newUser.name + ". Have fun!";
+         result.error = false;
+         result.username = newUser.name;
+         result.password = newUser.password;
 		} else
- 			res.message = "Sorry, we already have a member with that name.";
+ 			result.message = "Sorry, there is already a member with this name.";
 	}
-	return (reg);
+	return (result);
 }
 
 
-/**
- * check if all values necessary to register
- * are given and return them as properties of a
- * temporary Object
- */
-
-function checkReg() {
-   var reg = new Object();
-	if (!res.message) res.message = "Please fill out the form completely and then click the button to register.";
-   if (req.data.name)
-      reg.name = req.data.name;
-   else
-      reg.error = true;
-   // check if username contains any special characters
-   if (!isClean(reg.name)) {
-      res.message = "Please don't use any special characters in your username!";
-      reg.error = true;
-   }
-   // check if passwords match
-	if (req.data.password1 && req.data.password2) {
-      if (req.data.password1 == req.data.password2) {
-   	   reg.password1 = req.data.password1;
-         reg.password2 = req.data.password2;
-      } else {
-         res.message = "Passwords didn't match! Please re-enter:";
-         reg.error = true;
-      }
-   } else
-      reg.error = true;
-   // check if email-address is valid
-   if (req.data.email) {
-   	reg.email = req.data.email;
-      if (!checkEmail(req.data.email)) {
-         res.message = "Ooops! The email-address you entered is not valid!";
-         reg.error = true;
-      }
-   } else
-      reg.error = true;
-   if (req.data.description)
-      reg.description = req.data.description;
-   if (req.data.url)
-   	reg.url = evalURL(req.data.url);
-
-   return (reg);
-}
 
 /**
  * update user-profile
+ * @param Obj Object containing form values
+ * @return Obj Object containing two properties:
+ *             - error (boolean): true if error happened, false if everything went fine
+ *             - message (String): containing a message to user
  */
 
-function updateUser() {
-   if (req.data.oldpwd && req.data.newpwd1 && req.data.newpwd2) {
-      if (user.password != req.data.oldpwd) {
-         res.message = "Old password incorrect!";
-         res.redirect(this.href("edit"));
-      } else if (req.data.newpwd1 != req.data.newpwd2) {
-         res.message = "Ooops! Passwords didn't match! Please type in again!";
-         res.redirect(this.href("edit"));
+function updateUser(param) {
+   var result = new Object();
+   if (param.oldpwd && param.newpwd1 && param.newpwd2) {
+      if (user.password != param.oldpwd) {
+         result.message = "Old password is incorrect!";
+         result.error = true;
+      } else if (param.newpwd1 != param.newpwd2) {
+         result.message = "Ooops! Passwords didn't match! Please type in again!";
+         result.error = true;
       } else
-         user.password = req.data.newpwd1;
+         user.password = param.newpwd1;
    }
-   user.url = evalURL(req.data.url);
-   user.email = req.data.email;
-   user.description = req.data.description;
-   res.message = "Changes were saved successfully!";
-   res.redirect(this._parent.href());
+   if (!checkEmail(param.email)) {
+      result.message = "Your email-address is not valid!";
+      result.error = true;
+   }
+   if (!result.error) {
+      user.url = evalURL(param.url);
+      user.email = param.email;
+      // not in use right now: user.description = param.description;
+      result.message = "Changes were saved successfully!";
+   }
+   return (result);
 }
 
 /**
  * function retrieves a list of usernames/passwords for a submitted email-address
  * and sends them as mail
+ * @param String email-address to search for accounts
+ * @return Obj Object containing two properties:
+ *             - error (boolean): true if error happened, false if everything went fine
+ *             - message (String): containing a message to user
  */
 
-function sendPwd() {
-   if (!req.data.email)
-      return;
-   var email = req.data.email;
-   var sqlClause = "select USERNAME,PASSWORD from USER where EMAIL = '" + email + "'";
-   var dbConn = getDBConnection("antville");
-   var result = dbConn.executeRetrieval(sqlClause);
-   var cnt = 0;
-   var pwdList = "";
-   while (result.next()) {
-      pwdList += "Username: " + result.getColumnItem("USERNAME") + "\n";
-      pwdList += "Password: " + result.getColumnItem("PASSWORD") + "\n\n";
-      cnt++;
+function sendPwd(email) {
+   var result = new Object();
+   if (!email) {
+      result.message = "Please specify an email-address!";
+      result.error = true;
+   } else {
+      var sqlClause = "select USERNAME,PASSWORD from USER where EMAIL = '" + email + "'";
+      var dbConn = getDBConnection("antville");
+      var dbResult = dbConn.executeRetrieval(sqlClause);
+      var cnt = 0;
+      var pwdList = "";
+      while (dbResult.next()) {
+         pwdList += "Username: " + dbResult.getColumnItem("USERNAME") + "\n";
+         pwdList += "Password: " + dbResult.getColumnItem("PASSWORD") + "\n\n";
+         cnt++;
+      }
+      dbResult.release;
+      if (!cnt) {
+         result.message = "No accounts found for this eMail-address!";
+         result.error = true;
+      }
    }
-   result.release;
-   if (!cnt) {
-      res.message = "No accounts found for this eMail-address!";
-      return;
+   if (!result.error) {
+      // now we send the mail containing all accounts for this email-address
+      var mail = new Mail();
+      mail.setFrom(getProperty("adminEmail"));
+      mail.addTo(email);
+      mail.setSubject("Your Accounts for Antville");
+      var mailParam = new Object();
+      var now = new Date();
+      mailParam.timestamp = this._parent.formatTimestamp(now,new Object());
+      mailParam.text = pwdList;
+   	mail.setText(this.renderSkinAsString("pwdmail",mailParam));
+      var sendResult = mail.send();
+      if (sendResult.status) {
+         result.message = "An error occurred while trying to send the mail!";
+         result.error = true;
+      } else
+         result.message = "A mail containing your the password(s) was sent successfully!";
    }
-   // now we send the mail containing all accounts for this email-address
-   var mail = new Mail();
-   mail.setFrom(getProperty("adminEmail"));
-   mail.addTo(email);
-   mail.setSubject("Your Accounts for Antville");
-   var mailParam = new Object();
-   var now = new Date();
-   mailParam.timestamp = this._parent.formatTimestamp(now,new Object());
-   mailParam.text = pwdList;
-	mail.setText(this.renderSkinAsString("pwdmail",mailParam));
-   var result = mail.send();
-   if (result.status)
-      res.message = "An error occurred while trying to send the mail!";
-   else
-      res.message = "A mail containing your the password(s) was sent successfully!";
-   res.redirect(this._parent.href());
+   return (result);
 }
