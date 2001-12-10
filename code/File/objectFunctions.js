6821131c286ad1becbe03e4a66cb5ccfdddaa0f5
Robert Gaggl <robert@nomatic.org>
2001-12-10 22:51:57 +0000

    - removed unused functions - functions now have a defined (and documented ...) interface - added comments to functions

--- code/File/objectFunctions.js
+++ code/File/objectFunctions.js
@@ -1,49 +1,31 @@
 /**
- * save goodie as file on local disk
+ * function checks if new property-values for goodie are correct
+ * @param Obj Object containing form-values
+ * @param Obj User-Object modifying goodie
+ * @return Obj Object containing two properties:
+ *             - error (boolean): true if error happened, false if everything went fine
+ *             - message (String): containing a message to user
  */
 
-function saveGoodie(rawgoodie) {
-   if (rawgoodie && !rawgoodie.contentType) {
-      // whatever the user has uploaded, we won't accept it ;-)
-      this.cache.error = true; 
-      res.message = "What was that?"; 
-   } else {
-      rawgoodie.writeToFile(this.cache.saveTo,this.file);
-   } 
-   return; 
-}
-
-
-/** NOT IN USE!!!
- * function returns true if the mimetype of the goodie is acceptable
- * returns false if mimetype is unknown or now allowed
-
-function evalGoodieType(ct) {
-   if (ct.indexOf("audio") > -1)
-      return true;
-   return false;
-}
-*/
-
-/**
- * function checks if new image-parameters are correct ...
- */
-
-function evalGoodie() {
-   if (req.data.alias) {
-      if (req.data.alias != this.alias) {
-         if (this.weblog.goodies.get(req.data.alias)) {
+function evalGoodie(param,modifier) {
+   var result = new Object();
+   if (param.alias) {
+      if (param.alias != this.alias) {
+         if (this.weblog.goodies.get(param.alias)) {
             // alias has changed, but is already existing
-            res.message = "This name is already in use!";
-            res.redirect(this.href("edit"));
-         } else {
-            this.weblog.goodies.changeAlias(this);
-         }
+            result.message = "This name is already in use!";
+            result.error = true;
+         } else
+            this.weblog.goodies.changeAlias(this,param.alias);
       }
-      this.description = req.data.description;
-      res.message = "Changes saved successfully!";
-      res.redirect(this.weblog.goodies.href());
+      this.description = param.description;
+      this.modifier = modifier;
+      this.modifytime = new Date();
+      result.message = "Changes saved successfully!";
+      result.error = false;
+   } else {
+      result.message = "You must specify a name for this goodie!";
+      result.error = true;
    }
+   return (result);
 }
-
-
