f8fef3ebf280a3e12d4f296dcf5deb47b9c2f851
Robert Gaggl <robert@nomatic.org>
2001-12-12 14:02:28 +0000

    bugfix

--- antville_dbpatch20011208.sql
+++ antville_dbpatch20011208.sql
@@ -4,6 +4,8 @@ alter table WEBLOG
  add column  SMALLCOLOR varchar(6) null after SMALLFONT,
  add column  SMALLSIZE varchar(4) null after SMALLCOLOR;
 
+update WEBLOG set SMALLFONT = "Arial,Helvetica,sans-serif", SMALLCOLOR = "999999", SMALLSIZE = "8pt";
+
 alter table WEBLOG
  change column DATEFORMAT LONGDATEFORMAT varchar(50) null,
  add column  SHORTDATEFORMAT varchar(50) null after LONGDATEFORMAT;
