f9edfeef0858e4d69d6cbf183c919587b6494abc
Robert Gaggl <robert@nomatic.org>
2001-12-16 18:06:47 +0000

    added isStoryOnline-function

--- code/Site/objectFunctions.js
+++ code/Site/objectFunctions.js
@@ -281,3 +281,15 @@ function sortMostReads(s1, s2) {
 	else
 		return(0);
 }
+
+/**
+ * function checks if story is online in weblog
+ * @param Obj story to check
+ * @return Boolean true if online, false if not
+ */
+
+function isStoryOnline(st) {
+   if (parseInt(st.online,10) == 2)
+      return true;
+   return false;
+}
