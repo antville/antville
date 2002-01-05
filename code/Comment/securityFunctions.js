84266fa9149229914da22c882431f0dea9b7f127
Robert Gaggl <robert@nomatic.org>
2002-01-05 02:47:05 +0000

    removed functions inherited from story

--- code/Comment/securityFunctions.js
+++ code/Comment/securityFunctions.js
@@ -1,16 +1,4 @@
 /**
- * check if user is allowed to delete a comment
- * @param Obj Userobject
- * @return String Reason for denial (or null if allowed)
- */
-
-function isDeleteDenied(usr) {
-   if (!this.weblog.isUserAdmin(usr))
-      return ("You're not admin of this weblog, so you can't delete any postings!");
-   return null;
-}
-
-/**
  * check if user is allowed to edit a comment
  * @param Obj Userobject
  * @return String Reason for denial (or null if allowed)
@@ -22,43 +10,4 @@ function isEditDenied(usr) {
    else if (!this.weblog.hasDiscussions())
       return ("Sorry, discussions were disabled for this weblog!");
    return null;
-}
-
-/**
- * check if user is allowed to reply to a comment
- * @param Obj Userobject
- * @return String Reason for denial (or null if allowed)
- */
-
-function isReplyDenied(usr) {
-   if (!this.weblog.hasDiscussions())
-      return ("Sorry, discussions were disabled for this weblog!");
-   else if (!usr.uid) {
-      usr.cache.referer = this.href("reply");
-      return ("Please login first!");
-   } else if (usr.isBlocked())
-      return ("Sorry, your account was disabled!");
-   return null;
-}
-
-/**
- * function explicitly allowes some macros for use in the text of a story
- * @param Obj Skin-object to allow macros for
- */
-
-function allowTextMacros(s) {
-   s.allowMacro("image");
-   s.allowMacro("this.image");
-   s.allowMacro("weblog.image");
-   s.allowMacro("story.image");
-   s.allowMacro("thumbnail");
-   s.allowMacro("this.thumbnail");
-   s.allowMacro("weblog.thumbnail");
-   s.allowMacro("story.thumbnail");
-   s.allowMacro("link");
-   s.allowMacro("this.link");
-   s.allowMacro("weblog.link");
-   s.allowMacro("story.link");
-   s.allowMacro("goodie");
-   return;
 }
