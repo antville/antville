7c5441b54edd859c0ef171fb06778473e19d4b88
Robert Gaggl <robert@nomatic.org>
2002-01-05 02:33:07 +0000

    imagelist is now rendered in renderImagelist()-function, macro is left for backwards-compatibility

--- code/ImageMgr/macros.js
+++ code/ImageMgr/macros.js
@@ -1,22 +1,13 @@
 /**
- * macro renders imagepool as list
+ * macro renders res.data.imagelist to response-object
+ * left for backwards-compatibility only
  */
 
 function images_macro(param) {
    res.write(param.prefix)
-   var size = this.size();
-   var start = parseInt (req.data.start);
-   if (isNaN (start)|| start >= size-1)
-      start = 0;
-   var end = Math.min (start+20, size);
-   if (start > 0)
-      res.write ("...&nbsp;<a href='"+this.href()+"?start="+Math.max(0, start-20)+"'>newer images</a><br>");
-   for (var i=start; i<end; i++) {
-      this.get(i).renderSkin("preview");
-   }
-   if (end < size)
-      res.write ("<br><a href='"+this.href()+"?start="+end+"'>older images</a>&nbsp;...");
+   res.write(res.data.imagelist);
    res.write(param.suffix);
+   return;
 }
 
 
