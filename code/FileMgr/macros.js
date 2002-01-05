2832b15e6613b66f85616e78c0524860129bcaba
Robert Gaggl <robert@nomatic.org>
2002-01-05 02:32:44 +0000

    goodielist is now rendered in renderGoodielist()-function, macro is left for backwards-compatibility

--- code/FileMgr/macros.js
+++ code/FileMgr/macros.js
@@ -1,21 +1,12 @@
 /**
- * macro renders goodiepool as list
+ * macro renders res.data.goodielist to response-object
+ * left for backwards-compatibility only
  */
 
 function goodies_macro(param) {
    res.write(param.prefix)
-   var size = this.size();
-   var start = parseInt (req.data.start);
-   if (isNaN (start)|| start >= size-1)
-      start = 0;
-   var end = Math.min (start+20, size);
-   if (start > 0)
-      res.write ("...&nbsp;<a href='"+this.href()+"?start="+Math.max(0, start-20)+"'>newer goodies</a><br>");
-   for (var i=start; i<end; i++) {
-      this.get(i).renderSkin("preview");
-   }
-   if (end < size)
-      res.write ("<br><a href='"+this.href()+"?start="+end+"'>older goodies</a>&nbsp;...");
+   res.write(res.data.goodielist);
    res.write(param.suffix);
+   return;
 }
 
