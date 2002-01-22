0cb53d80842950f26390439e6b652dc8b763f577
Robert Gaggl <robert@nomatic.org>
2002-01-22 22:24:08 +0000

    fixed editableby-column

--- antville_dbpatch20020109.sql
+++ antville_dbpatch20020109.sql
@@ -3,3 +3,4 @@ alter table WEBLOG drop column USERMAYSIGNUP;
 alter table MEMBER change column LEVEL LEVEL mediumint(10) null;
 update MEMBER set LEVEL = 131071 where LEVEL = 2;
 update MEMBER set LEVEL = 9361 where LEVEL = 1;
+update STORY set EDITABLEBY = null where EDITABLEBY > 1;
