e395ff7767e999e73017bf07646b48ea93c61b40
Robert Gaggl <robert@nomatic.org>
2002-01-22 20:09:25 +0000

    - removed functions isAdmin(), isContributor() - some more changes needed due to new role-scheme

--- code/Membership/objectFunctions.js
+++ code/Membership/objectFunctions.js
@@ -1,25 +1,4 @@
 /**
- * function returns true if the member has admin-status
- */
-
-function isAdmin() {
-   if (parseInt(this.level,10) == 2)
-      return true;
-   return false;
-}
-
-/**
- * function returns true if the member has contributor-status
- */
-
-function isContributor() {
-   if (parseInt(this.level,10) == 1)
-      return true;
-   return false;
-}
-
-
-/**
  * function updates a membership
  * @param Int Integer representing role of user
  * @param Obj User-object modifying this member
@@ -31,10 +10,17 @@ function isContributor() {
 function updateMember(lvl,modifier) {
    var result = new Object();
    if (!isNaN(lvl)) {
-      this.level = lvl;
+      if (lvl == 1)
+         this.level = getContributorLvl();
+      else if (lvl == 2)
+         this.level = getContentManagerLvl();
+      else if (lvl == 3)
+         this.level = getAdminLvl();
+      else
+         this.level = 0;
       this.modifytime = new Date();
       this.modifier = modifier;
-      this.sendConfirmationMail(modifier.email);
+      // FIXME: disabled for testing: this.sendConfirmationMail(modifier.email);
       result.message = "Changes were saved successfully!";
       result.error = false;
    } else {
