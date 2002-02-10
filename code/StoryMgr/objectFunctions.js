15b70b86846889b91b7e3f09b3334a0114ccddeb
Robert Gaggl <robert@nomatic.org>
2002-02-10 16:36:21 +0000

    rewrote evalNewStory()

--- code/StoryMgr/objectFunctions.js
+++ code/StoryMgr/objectFunctions.js
@@ -1,54 +1,74 @@
 /**
  * function checks if story fits to the minimal needs (must have at least a text ;-)
+ * @param Obj story-object to work on
  * @param Obj Object containing the properties needed for creating a new story
  * @param Obj User-Object creating this story
  * @return Obj Object containing three properties:
  *             - error (boolean): true if error happened, false if everything went fine
  *             - message (String): containing a message to user
+ *             - story (Obj): story-object containing assigned form-values
  *             - id (Int): id of created story
  */
 
-function evalNewStory(param,creator) {
+function evalNewStory(s,param,creator) {
    var result = new Object();
-   if (param.text && creator) {
-      result.url = this.href();
-      var newStory = new story();
-      newStory.prototype = "story";
-      var online = parseInt(param.online,10);
-      var editableby = parseInt(param.editableby,10);
-      newStory.weblog = this._parent;
-      newStory.title = param.title;
-      newStory.text = param.text;
-      if (param.newtopic)
-         newStory.topic = param.newtopic;
-      else if (parseInt(param.topic,10) > 0)
-         newStory.topic = this._parent.topics.get(parseInt(param.topic,10) -1).groupname;
-      if (isNaN(online) || (online == 1 && !newStory.topic))
-         newStory.online = 0;
-      else
-         newStory.online = online;
-      newStory.editableby = !isNaN(editableby) ? editableby : null;
-      newStory.author = creator;
-      newStory.createtime = new Date();
-      newStory.modifytime = new Date();
-      newStory.day = newStory.createtime.format("yyyyMMdd");
-      newStory.ipaddress = param.http_remotehost;
-      newStory.reads = 0;
-      this._parent.add(newStory);
-      if (newStory.online) {
-         this._parent.lastupdate = newStory.createtime;
-         if (newStory.topic)
-            result.url = this._parent.topics.href() + escape(newStory.topic);
-         else
-            result.url = newStory.href();
-      } else
-         result.url = this.href();
-      result.message = "The story was created successfully!";
-      result.id = newStory._id;
-      result.error = false;
-   } else {
-      result.message = "Please fill out the form to create a new story";
+   result.error = false;
+   result.story = s;
+   s.weblog = this._parent;
+   s.title = param.title;
+   s.text = param.text;
+   s.prototype = "story";
+   s.author = creator;
+   s.modifytime = s.createtime = new Date();
+   s.editableby = !isNaN(parseInt(param.editableby)) ? parseInt(param.editableby,10) : null;
+   s.day = s.createtime.format("yyyyMMdd");
+   s.ipaddress = param.http_remotehost;
+   s.reads = 0;
+   // start checking if story-params make sense
+   if (!s.text) {
+      result.message = "You need at least some text!";
       result.error = true;
+   }
+
+   var topic = param.topic ? param.topic : parseInt(param.topicidx,10);
+   if (!isNaN(topic) && topic >= 0) {
+      var topicNode = this._parent.topics.get(topic);
+      s.topic = topicNode ? topicNode.groupname : null;
+   } else
+      s.topic = topic ? topic : null;
+
+   // check if topic-name contains any forbidden characters
+   if (!isCleanForURL(s.topic)) {
+      result.message = "The name of the topic contains forbidden characters!";
+      result.error = true;
+   }
+   var status = parseInt(param.online,10);
+   if (isNaN(status)) {
+      result.message = "Status of story is missing!";
+      result.error = true;
+   } else if (status == 1 && !s.topic) {
+      result.message = "Can't set story online in topic because you didn't specify one!";
+      result.error = true;
+   } else
+      s.online = status;
+
+   // if everything ok, so proceed with adding the story
+   if (!result.error) {
+      if (this._parent.add(s)) {
+         if (s.online) {
+            this._parent.lastupdate = s.createtime;
+            if (s.topic)
+               result.url = this._parent.topics.href() + escape(s.topic);
+            else
+               result.url = s.href();
+         } else
+            result.url = this.href();
+         result.message = "The story was created successfully!";
+         result.id = newStory._id;
+      } else {
+         result.message = "Couldn't add the story!";
+         result.error = true;
+      }
    }
    return (result);
 }
