dc59977c54110c9fd366fcac9ca2c5a1a89efd2f
Tobi Schäfer <interface@p3k.org>
2002-04-24 12:11:19 +0000

    initial check-in

---
+++ antville_dbpatch20020424.sql
@@ -0,0 +1,9 @@
+use antville;
+
+alter table ACCESS
+change column REFERRER REFERRER text null, 
+change column IP IP varchar(20) null, 
+change column URL URL text null, 
+change column PATH PATH varchar(255) null, 
+change column `ACTION` `ACTION` varchar(50) null, 
+change column BROWSER BROWSER varchar(255) null;
