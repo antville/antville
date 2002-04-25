8800b3783129fd1ebbc5e565a2113d5b88503f32
Tobi Schäfer <interface@p3k.org>
2002-04-25 12:11:28 +0000

    corrected syntax (colon instead of semicolon before add statement)

--- antville_dbpatch20020424.sql
+++ antville_dbpatch20020424.sql
@@ -1,11 +1,10 @@
 use antville;
 
 alter table ACCESS
-change column REFERRER REFERRER text null, 
-change column IP IP varchar(20) null, 
-change column URL URL text null, 
-change column PATH PATH varchar(255) null, 
-change column `ACTION` `ACTION` varchar(50) null, 
-change column BROWSER BROWSER varchar(255) null;
-
-add column STORY_ID bigint(20) not null after WEBLOG_ID;
+change column REFERRER REFERRER text null,
+change column IP IP varchar(20) null,
+change column URL URL text null,
+change column PATH PATH varchar(255) null,
+change column `ACTION` `ACTION` varchar(50) null,
+change column BROWSER BROWSER varchar(255) null,
+add column STORY_ID bigint(20) null after WEBLOG_ID;
