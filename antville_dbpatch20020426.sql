c4eb14cf02dd86d0e81050c51926e0ac8ceb2df9
Robert Gaggl <robert@nomatic.org>
2002-04-26 13:08:16 +0000

    added modification of ACCESS-table (columns URL,PATH and ACTION are dropped)

--- antville_dbpatch20020426.sql
+++ antville_dbpatch20020426.sql
@@ -2,3 +2,7 @@ alter table WEBLOG
 add column  LASTPING datetime null after LASTDELWARN,
 add column  ENABLEPING tinyint(1) null after LASTPING;
 
+alter table ACCESS
+drop column URL,
+drop column PATH,
+drop column `ACTION`;
