04e9deb6a0f99d7f54994552a69ba78a67bfa4a9
Hannes Wallnöfer <hannesw@gmail.com>
2002-05-15 13:04:58 +0000

    check if there is a path.weblog before accessing it (twice)

--- code/HopObject/securityFunctions.js
+++ code/HopObject/securityFunctions.js
@@ -1,4 +1,4 @@
-/**
+</**
  * function checks if there's a weblog in path
  * if true it checks if the weblog or the user is blocked
  */
@@ -6,13 +6,14 @@
 function onRequest() {
    autoLogin();
    // defining skinpath
-   res.skinpath = new Array(path.weblog.skinmanager);
+   if (path.weblog)
+      res.skinpath = new Array(path.weblog.skinmanager);
    if (path.weblog && path.weblog.isBlocked())
       res.redirect(root.href("blocked"));
    if (user.isBlocked()) {
       // user was blocked recently, so log out
       user.logout();
       res.message = "Your account was blocked!";
-      res.redirect(path.weblog.weblog ? path.weblog.href() : root.href());
+      res.redirect(path.weblog ? path.weblog.href() : root.href());
    }
 }
