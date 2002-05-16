a41cbd2e8cd9cde433e4b583091ff231a00542b4
Robert Gaggl <robert@nomatic.org>
2002-05-16 12:28:15 +0000

    removed getActionName() which got replaced with built-in req.action

--- code/Global/renderFunctions.js
+++ code/Global/renderFunctions.js
@@ -1,21 +1,5 @@
 
 /**
- * returns name of action file the user has called
- * Input params: none
- * returns: string (= name of .hac-file)
- */
-
-
-function getActionName() {
-   // return name of current action executed by user
-   var rPath = req.path.split("/");
-   if (path[path.length-1]._id == rPath[rPath.length-1] || path[path.length-1]._name == rPath[rPath.length-1])
-      return "main";
-   else
-      return(rPath[rPath.length -1]);
-}
-
-/**
  * function tries to check if the color contains just hex-characters
  * if so, it renders the color-definition prefixed with a '#'
  * otherwise it assumes the color is a named one
