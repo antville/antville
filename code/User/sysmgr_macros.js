b1322e4b142fdd8f56a0f644facc0c1c91e10da8
Robert Gaggl <robert@nomatic.org>
2002-06-02 15:27:35 +0000

    now using new global function isUserSysAdmin() removed explicit writing of prefix and suffix (helma's doing that from now on) change code according to helma's new session/usermodel: - using session.user instead of user as argument

--- code/User/sysmgr_macros.js
+++ code/User/sysmgr_macros.js
@@ -6,9 +6,8 @@ function sysmgr_count_macro(param) {
    if (!param || !param.what)
       return;
    // this macro is allowed just for sysadmins
-   if (!user.isSysAdmin())
+   if (!isUserSysAdmin())
       return;
-   res.write(param.prefix);
    if (param.what == "stories")
       res.write(this.stories.size());
    else if (param.what == "comments")
@@ -17,7 +16,6 @@ function sysmgr_count_macro(param) {
       res.write(this.images.size());
    else if (param.what == "goodies")
       res.write(this.goodies.size());
-   res.write(param.suffix);
 }
 
 /**
@@ -26,16 +24,14 @@ function sysmgr_count_macro(param) {
 
 function sysmgr_statusflags_macro(param) {
    // this macro is allowed just for sysadmins
-   if (!user.isSysAdmin())
+   if (!isUserSysAdmin())
       return;
-   res.write(param.prefix);
    if (this.isTrusted())
       res.write("<span class=\"flagltgreen\" nowrap>TRUSTED</span>");
    if (this.isSysAdmin())
       res.write("<span class=\"flagdkgreen\" nowrap>SYSADMIN</span>");
    if (this.isBlocked())
       res.write("<span class=\"flagblack\" nowrap>BLOCKED</span>");
-   res.write(param.suffix);
 }
 
 /**
@@ -44,9 +40,8 @@ function sysmgr_statusflags_macro(param) {
 
 function sysmgr_editlink_macro(param) {
    // this macro is allowed just for sysadmins
-   if (!user.isSysAdmin() || req.data.edit == this.name || user == this)
+   if (!isUserSysAdmin() || req.data.edit == this.name || session.user == this)
       return;
-   res.write(param.prefix);
    var linkParam = new Object();
    linkParam.linkto = "users";
    linkParam.urlparam = "?item=" + this.name;
@@ -57,7 +52,6 @@ function sysmgr_editlink_macro(param) {
    root.manage.openLink(linkParam);
    res.write(param.text ? param.text : "edit");
    this.closeLink();
-   res.write(param.suffix);
 }
 
 /**
@@ -65,9 +59,7 @@ function sysmgr_editlink_macro(param) {
  */
 
 function sysmgr_username_macro(param) {
-   res.write(param.prefix);
    res.write(this.name);
-   res.write(param.suffix);
 }
 
 /**
@@ -76,12 +68,10 @@ function sysmgr_username_macro(param) {
 
 function sysmgr_registered_macro(param) {
    // this macro is allowed just for sysadmins
-   if (!user.isSysAdmin())
+   if (!isUserSysAdmin())
       return;
-   res.write(param.prefix);
    fmt=param.format ? param.format : "dd.MM.yyyy HH:mm";
    res.write(this.registered.format(fmt));
-   res.write(param.suffix);
 }
 
 /**
@@ -90,14 +80,12 @@ function sysmgr_registered_macro(param) {
 
 function sysmgr_lastvisit_macro(param) {
    // this macro is allowed just for sysadmins
-   if (!user.isSysAdmin())
+   if (!isUserSysAdmin())
       return;
-   res.write(param.prefix);
    if (this.lastvisit) {
       fmt=param.format ? param.format : "dd.MM.yyyy HH:mm";
       res.write(this.lastvisit.format(fmt));
    }
-   res.write(param.suffix);
 }
 
 /**
@@ -106,16 +94,14 @@ function sysmgr_lastvisit_macro(param) {
 
 function sysmgr_trusted_macro(param) {
    // this macro is allowed just for sysadmins
-   if (!user.isSysAdmin())
+   if (!isUserSysAdmin())
       return;
-   res.write(param.prefix);
    if (param.as == "editor") {
       var options = new Array("no","yes");
       var selectedIndex = parseInt(this.trusted,10);
       res.write(simpleDropDownBox("trusted",options,selectedIndex));
    } else
       res.write(this.isTrusted() ? "yes" : "no");
-   res.write(param.suffix);
 }
 
 /**
@@ -124,16 +110,14 @@ function sysmgr_trusted_macro(param) {
 
 function sysmgr_blocked_macro(param) {
    // this macro is allowed just for sysadmins
-   if (!user.isSysAdmin())
+   if (!isUserSysAdmin())
       return;
-   res.write(param.prefix);
    if (param.as == "editor") {
       var options = new Array("no","yes");
       var selectedIndex = parseInt(this.blocked,10);
       res.write(simpleDropDownBox("blocked",options,selectedIndex));
    } else
       res.write(this.isBlocked() ? "yes" : "no");
-   res.write(param.suffix);
 }
 
 /**
@@ -142,14 +126,12 @@ function sysmgr_blocked_macro(param) {
 
 function sysmgr_sysadmin_macro(param) {
    // this macro is allowed just for sysadmins
-   if (!user.isSysAdmin())
+   if (!isUserSysAdmin())
       return;
-   res.write(param.prefix);
    if (param.as == "editor") {
       var options = new Array("no","yes");
       var selectedIndex = parseInt(this.sysadmin,10);
       res.write(simpleDropDownBox("sysadmin",options,selectedIndex));
    } else
       res.write(this.isSysAdmin() ? "yes" : "no");
-   res.write(param.suffix);
 }
