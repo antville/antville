a8599fa882546efe505e8f22cc98a6ee70246f5c
Tobi Schäfer <interface@p3k.org>
2002-06-03 16:59:48 +0000

    new and improved function createLinkParam()

--- code/HopObject/objectFunctions.js
+++ code/HopObject/objectFunctions.js
@@ -20,16 +20,48 @@ function getNavigationName () {
    return this._name;
 }
 
-/**
- * creates parameter-object that will be passed to
- * function that renders the input
- */
 
-function createInputParam(propName,param) {
+/**
+ * creates parameter object that will be passed to
+ * function that renders the input element
+ */
+function createInputParam(propName, param) {
    var inputParam = new Object();
    inputParam.name = propName;
    for (var i in param)
       inputParam[i] = param[i];
    inputParam.value = this[propName];
    return (inputParam);
+}
+
+
+/**
+ * derives parameter object from an object that will
+ * be passed to function that renders the link element
+ */
+function createLinkParam(param) {
+  var url = param.to ? param.to : param.linkto;
+  if (!url || url == "main")
+    param.href = this.href();
+  else if (url.indexOf("://") > -1 || url.substring(0, 10) == "javascript")
+    param.href = url;
+  else {
+    // check if link points to a subcollection
+    if (url.indexOf("/") > -1)
+      param.href = this.href() + url;
+    else
+      param.href = this.href(url);
+  }
+  if (param.urlparam)
+    param.href += param.urlparam;
+  if (param.anchor)
+    param.href += param.anchor;
+  delete param.to;
+  delete param.linkto;
+  delete param.urlparam;
+  delete param.anchor;
+  delete param.text;
+  delete param.prefix;
+  delete param.suffix;
+  return(param);
 }
