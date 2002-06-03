9cd45dea94f3e11f0a10615d45c529b6380a9413
Tobi Schäfer <interface@p3k.org>
2002-06-03 16:54:34 +0000

    removed link_macro() and moved check for access permission (param.checkdeny) to hopobject/macros.js:link_macro()

--- code/PollMgr/macros.js
+++ code/PollMgr/macros.js
@@ -7,18 +7,3 @@ function pollList_macro(param) {
    res.write(res.data.pollList);
    return;
 }
-
-
-function link_macro(param) {
-	if (param.checkdeny == "true") {
-		if (this.isDenied(session.user))
-			return("");
-	}
-
-	this.openLink(param);
-	if (param.text)
-	  res.write(param.text);
-	else
-	  res.write(param.to ? param.to : param.linkto);
-	this.closeLink();
-}
