15b1b6b3122660eb96838c9de47ccb33fc55cd92
Robert Gaggl <robert@nomatic.org>
2002-06-26 17:05:44 +0000

    - changed because the prototype weblog was renamed to site - replaced hardcoded messages with getMsg()

--- code/User/objectFunctions.js
+++ code/User/objectFunctions.js
@@ -7,7 +7,7 @@ function sendConfirmationMail(sender) {
    var mail = new Mail();
    mail.setFrom(sender);
    mail.addTo(this.email);
-   mail.setSubject("Welcome to Antville!");
+   mail.setSubject(getMsg("mailsubject","registration",root.getSysTitle()));
    var mailParam = new Object();
    mailParam.name = this.name;
    mailParam.password = this.password;
@@ -48,13 +48,13 @@ function isSysAdmin() {
 
 /**
  * function for sorting member-objects by the lastupdate-timestamp
- * of the according weblog
+ * of the according site
  */
 
 function sortSubscriptions(s1,s2) {
-   if (s1.weblog.lastupdate < s2.weblog.lastupdate)
+   if (s1.site.lastupdate < s2.site.lastupdate)
       return 1;
-   else if (s1.weblog.lastupdate > s2.weblog.lastupdate)
+   else if (s1.site.lastupdate > s2.site.lastupdate)
       return -1;
    else
       return 0;
