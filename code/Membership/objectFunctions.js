701ba3af19f59c77b05016133e803356ce0cc1d6
Robert Gaggl <robert@nomatic.org>
2002-06-26 16:59:10 +0000

    changed formatting, replaced hardcoded mail-subject with call of getMsg()

--- code/Membership/objectFunctions.js
+++ code/Membership/objectFunctions.js
@@ -1,30 +1,30 @@
 /**
  * function updates a membership
  * @param Int Integer representing role of user
- * @param Obj User-object modifying this member
+ * @param Obj User-object modifying this membership
  * @return Obj Object containing two properties:
  *             - error (boolean): true if error happened, false if everything went fine
  *             - message (String): containing a message to user
  */
 
-function updateMember(lvl,modifier) {
-   var result;
-   if (!isNaN(lvl)) {
-      if (lvl == 1)
-         this.level = getContributorLvl();
-      else if (lvl == 2)
-         this.level = getContentManagerLvl();
-      else if (lvl == 3)
-         this.level = getAdminLvl();
-      else
-         this.level = 0;
-      this.modifytime = new Date();
-      this.modifier = modifier;
-      this.sendConfirmationMail(modifier.email);
-      result = getConfirm("update");
-   } else
-      result = getError("memberNoRole");
-   return (result);
+function updateMembership(lvl,modifier) {
+   if (isNaN(lvl))
+      return (getError("memberNoRole"));
+   // editing the own membership is denied
+   if (this.user == modifier)
+      return (getError("memberEditSelfDenied"));
+   if (lvl == 1)
+      this.level = getContributorLvl();
+   else if (lvl == 2)
+      this.level = getContentManagerLvl();
+   else if (lvl == 3)
+      this.level = getAdminLvl();
+   else
+      this.level = 0;
+   this.modifytime = new Date();
+   this.modifier = modifier;
+   this.sendConfirmationMail(modifier.email);
+   return (getConfirm("update"));
 }
 
 /**
@@ -36,7 +36,7 @@ function sendConfirmationMail(fromEmail) {
 	var mail = new Mail();
 	mail.setFrom(fromEmail);
 	mail.addTo(this.user.email);
-	mail.setSubject(this.weblog.title + " - Your status was changed!");
+	mail.setSubject(getMsg("mailsubject","statusChange",this.site.title));
 	mail.setText(this.renderSkinAsString("mailbody"));
 	mail.send();
 }
