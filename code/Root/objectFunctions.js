dda33834aa5c3480f1c21cd37026ef2835717e2a
Robert Gaggl <robert@nomatic.org>
2002-06-26 17:03:26 +0000

    - renamed some functions and variables because the prototype weblog was renamed to site - added functions getLocale(), getSysUrl(), getSysTitle() - fixed direct database-query to match new db-schema

--- code/Root/objectFunctions.js
+++ code/Root/objectFunctions.js
@@ -1,30 +1,29 @@
 /**
- * evaluating new weblog
+ * evaluating new site
  */
 
-function evalWeblog(title,alias,creator) {
+function evalSite(title,alias,creator) {
    var result;
    if (!alias)
-      result = getError("weblogAliasMissing");
+      result = getError("siteAliasMissing");
    else if (this.get(alias))
-      result = getError("weblogAliasExisting");
+      result = getError("siteAliasExisting");
    else if (!isClean(alias))
-      result = getError("weblogAliasNoSpecialChars");
+      result = getError("siteAliasNoSpecialChars");
    else {
       // check if alias is similar to an action-name (which is reserved)
-      var reserved = eval("this." + alias + "_action");
-      if (reserved)
-         result = getError("weblogAliasReserved");
+      if (this[alias] || this[alias + "_action"])
+         result = getError("siteAliasReserved");
    }
    if (!title)
-      result = getError("weblogTitleMissing");
+      result = getError("siteTitleMissing");
    if (!result) {
-      var newWeblog = this.createNewWeblog(title,alias,creator);
-      if (!newWeblog)
-         result = getError("weblogCreate");
+      var newSite = this.createNewSite(title,alias,creator);
+      if (!newSite)
+         result = getError("siteCreate");
       else {
-         result = getConfirm("weblogCreate");
-         result.weblog = newWeblog;
+         result = getConfirm("siteCreate");
+         result.site = newSite;
       }
    }
    return (result);
@@ -32,74 +31,76 @@ function evalWeblog(title,alias,creator) {
 
 
 /**
- * create a new weblog
+ * create a new site
  */
 
-function createNewWeblog(title,alias,creator) {
-   var newLog = new weblog();
-   newLog.title = title;
-   newLog.alias = alias;
-   newLog.creator = creator;
-   newLog.createtime = newLog.lastoffline = newLog.birthdate = new Date();
-   newLog.email = creator.email;
-   newLog.online = 0;
-   newLog.discussions = 1;
-   newLog.usercontrib = 0;
-   newLog.usersignup = 1;
-   newLog.archive = 1;
-   newLog.blocked = 0;
-   newLog.trusted = (creator.isTrusted() ? 1 : 0);
-   newLog.bgcolor = "FFFFFF";
-   newLog.textfont = "Verdana,Helvetica,Arial,sans-serif";
-   newLog.textsize = "13px";
-   newLog.textcolor = "000000";
-   newLog.linkcolor = "FF3300";
-   newLog.alinkcolor = "FF0000";
-   newLog.vlinkcolor = "FF3300";
-   newLog.titlefont = "Verdana,Helvetica,Arial,sans-serif";
-   newLog.titlesize = "15px";
-   newLog.titlecolor = "CC0000";
-   newLog.smallfont = "Arial,Helvetica,sans-serif";
-   newLog.smallsize = "12px";
-   newLog.smallcolor = "999999";
-   newLog.days = 3;
-   newLog.language = "en";
-   newLog.country = "US";
-   newLog.longdateformat = "EEEE, dd. MMMM yyyy, h:mm a";
-   newLog.shortdateformat = "yyyy.MM.dd, HH:mm";
-   newLog.enableping = 0;
-   newLog.createImgDirectory()
-   if (this.add(newLog)) {
-      // create member-object for connecting user <-> weblog with admin-rights
-      newLog.members.addMember(creator,getAdminLvl());
-      return (newLog);
+function createNewSite(title,alias,creator) {
+   var newSite = new site();
+   newSite.title = title;
+   newSite.alias = alias;
+   newSite.creator = creator;
+   newSite.createtime = newSite.lastoffline = new Date();
+   newSite.email = creator.email;
+   newSite.online = 0;
+   newSite.discussions = 1;
+   newSite.usercontrib = 0;
+   newSite.usersignup = 1;
+   newSite.archive = 1;
+   newSite.blocked = 0;
+   newSite.trusted = (creator.isTrusted() ? 1 : 0);
+   newSite.bgcolor = "FFFFFF";
+   newSite.textfont = "Verdana,Helvetica,Arial,sans-serif";
+   newSite.textsize = "13px";
+   newSite.textcolor = "000000";
+   newSite.linkcolor = "FF3300";
+   newSite.alinkcolor = "FF0000";
+   newSite.vlinkcolor = "FF3300";
+   newSite.titlefont = "Verdana,Helvetica,Arial,sans-serif";
+   newSite.titlesize = "15px";
+   newSite.titlecolor = "CC0000";
+   newSite.smallfont = "Arial,Helvetica,sans-serif";
+   newSite.smallsize = "12px";
+   newSite.smallcolor = "999999";
+   newSite.days = 3;
+   // retrieve locale-object from root (either specified or default-locale from JVM)
+   var loc = this.getLocale();
+   newSite.language = loc.getLanguage();
+   newSite.country = loc.getCountry();
+   newSite.longdateformat = "EEEE, dd. MMMM yyyy, h:mm a";
+   newSite.shortdateformat = "yyyy.MM.dd, HH:mm";
+   newSite.enableping = 0;
+   newSite.createImgDirectory()
+   if (this.add(newSite)) {
+      // create membership-object for connecting user <-> site with admin-rights
+      newSite.members.addMembership(creator,getAdminLvl());
+      return (newSite);
    } else
       return null;
 }
 
 /**
- * function removes a weblog completely
+ * function removes a site completely
  * including stories, comments, members
- * @param Object weblog to remove
+ * @param Object site to remove
  */
 
-function deleteWeblog(weblog) {
-   weblog.deleteAll();
-   this.remove(weblog);
+function deleteSite(site) {
+   site.deleteAll();
+   this.remove(site);
    // add syslog-entry
-   this.manage.syslogs.add(new syslog("weblog",weblog.alias,"removed weblog",session.user));
-   return (getConfirm("weblogDelete",weblog.alias));
+   this.manage.syslogs.add(new syslog("site",site.alias,"removed site",session.user));
+   return (getConfirm("siteDelete",site.alias));
 }
 
 /**
- *  Search one or more (public) weblogs. Returns an array containing weblog-aliases and
+ *  Search one or more (public) sites. Returns an array containing site-aliases and
  *  story ids of matching items.
  *
  * @param query The unprocessed query string
- * @param wlogid ID of weblog in which to search, null searches all
+ * @param sid ID of site in which to search, null searches all
  * @return The result array
  */
-function searchWeblogs (query, wlogid) {
+function searchSites (query, sid) {
 
     // reuse search result container if it already exists
     var result = result = new Array();
@@ -110,19 +111,19 @@ function searchWeblogs (query, wlogid) {
     var qarr = query.replace(unquote, "''").split(" ");
 
     // construct query
-    var where = "select TEXT.ID, WEBLOG.ALIAS from TEXT, WEBLOG where "+
-                "TEXT.WEBLOG_ID = WEBLOG.ID and "+
-                "TEXT.ISONLINE > 0 and WEBLOG.ISONLINE > 0 and ";
+    var where = "select AV_TEXT.TEXT_ID, AV_SITE.SITE_ALIAS from AV_TEXT, AV_SITE where "+
+                "AV_TEXT.TEXT_F_SITE = AV_SITE.SITE_ID and "+
+                "AV_TEXT.TEXT_ISONLINE > 0 and AV_SITE.SITE_ISONLINE > 0 and ";
     for (var i in qarr) {
-        where += "(LOWER(TEXT.TITLE) like '%"+qarr[i].toLowerCase()+
-                 "%' or LOWER(TEXT.TEXT) like '%"+qarr[i].toLowerCase()+"%') "
+        where += "(LOWER(AV_TEXT.TEXT_TITLE) like '%"+qarr[i].toLowerCase()+
+                 "%' or LOWER(AV_TEXT.TEXT_TEXT) like '%"+qarr[i].toLowerCase()+"%') "
         if (i < qarr.length-1)
             where += "and ";
     }
-    // search only in the specified weblgs
-    if (wlogid)
-        where += "and WEBLOG.ID = "+wlogid+" ";
-    where += "order by TEXT.CREATETIME desc";
+    // search only in the specified site
+    if (sid)
+        where += "and AV_SITE.SITE_ID = "+sid+" ";
+    where += "order by AV_TEXT.TEXT_CREATETIME desc";
     // writeln (where);
     var dbcon = getDBConnection ("antville");
     var dbres = dbcon.executeRetrieval(where);
@@ -130,10 +131,52 @@ function searchWeblogs (query, wlogid) {
        while (dbres.next()) {
           var item = new Object();
           item.sid = dbres.getColumnItem (1).toString();
-          item.weblogalias = dbres.getColumnItem (2);
+          item.sitealias = dbres.getColumnItem (2);
           result[result.length] = item;
        }
     }
     dbres.release();
     return result;
+}
+
+/**
+ * function checks if language and country were specified
+ * for root. if so, it returns the specified Locale-object
+ * otherwise it returns the default locale of the JVM
+ */
+
+function getLocale() {
+   var locale = this.cache.locale;
+   if (locale) 
+       return locale;
+   if (this.sys_language)
+      locale = new java.util.Locale(this.sys_language,this.sys_country ? this.sys_country : "");
+   else
+      locale = java.util.Locale.getDefault();
+   this.cache.locale =locale;
+   return locale;
+}
+
+/**
+ * function checks if the system title of this antville-installation
+ * was defined in setup
+ * if not, it returns "Antville"
+ */
+
+function getSysTitle() {
+   if (!root.sys_title)
+      return ("Antville");
+   return (root.sys_title);
+}
+
+/**
+ * function checks if the system url of this antville-installation
+ * was defined in setup
+ * if not, it returns "http://www.antville.org"
+ */
+
+function getSysUrl() {
+   if (!root.sys_url)
+      return ("http://www.antville.org");
+   return (root.sys_url);
 }
