10dfb1d116c9f15675ad44349d103580845bc48f
Robert Gaggl <robert@nomatic.org>
2002-06-26 16:56:45 +0000

    changes needed because weblog was renamed to site, goodie to file and goodiemgr to filemgr

--- code/File/objectFunctions.js
+++ code/File/objectFunctions.js
@@ -1,27 +1,27 @@
 /**
- * function checks if new property-values for goodie are correct
+ * function checks if new property-values for a file are correct
  * @param Obj Object containing form-values
- * @param Obj User-Object modifying goodie
+ * @param Obj User-Object modifying file
  * @return Obj Object containing two properties:
  *             - error (boolean): true if error happened, false if everything went fine
  *             - message (String): containing a message to user
  */
 
-function evalGoodie(param,modifier) {
+function evalFile(param,modifier) {
    var result;
    if (param.alias) {
       if (param.alias != this.alias) {
-         if (this.weblog.goodies.get(param.alias)) {
+         if (this.site.files.get(param.alias)) {
             // alias has changed, but is already existing
             result = getError("nameInUse");
          } else
-            this.weblog.goodies.changeAlias(this,param.alias);
+            this.site.files.changeAlias(this,param.alias);
       }
       this.description = param.description;
       this.modifier = modifier;
       this.modifytime = new Date();
       result = getConfirm("update");
    } else
-      result = getError("goodieNameMissing");
+      result = getError("fileNameMissing");
    return (result);
 }
