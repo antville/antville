0718de309729462f5e9ed72af832c79a792dbe07
Robert Gaggl <robert@nomatic.org>
2002-06-26 17:27:13 +0000

    changed because - the prototype weblog was renamed to site - the collection "goodies" was renamed to "files"

--- code/Site/sysmgr_macros.js
+++ code/Site/sysmgr_macros.js
@@ -14,12 +14,12 @@ function sysmgr_count_macro(param) {
       res.write(this.allcontent.size() - this.allstories.size());
    else if (param.what == "images")
       res.write(this.images.size());
-   else if (param.what == "goodies")
-      res.write(this.goodies.size());
+   else if (param.what == "files")
+      res.write(this.files.size());
 }
 
 /**
- * function renders the statusflags for this weblog
+ * function renders the statusflags for this site
  */
 
 function sysmgr_statusflags_macro(param) {
@@ -44,7 +44,7 @@ function sysmgr_editlink_macro(param) {
    // this macro is allowed just for sysadmins
    if (!isUserSysAdmin() || req.data.item == this.alias)
       return;
-   param.linkto = "weblogs";
+   param.linkto = "sites";
    param.urlparam = "item=" + this.alias + "&action=edit";
    if (req.data.page)
       param.urlparam += "&page=" + req.data.page;
@@ -62,7 +62,7 @@ function sysmgr_deletelink_macro(param) {
    // this macro is allowed just for sysadmins
    if (!isUserSysAdmin() || req.data.item == this.alias)
       return;
-   param.linkto = "weblogs";
+   param.linkto = "sites";
    param.urlparam = "item=" + this.alias + "&action=remove";
    if (req.data.page)
       param.urlparam += "&page=" + req.data.page;
@@ -74,7 +74,7 @@ function sysmgr_deletelink_macro(param) {
 
 /**
  * macro renders the name of the creator of this
- * weblog as link
+ * site as link
  */
 
 function sysmgr_creator_macro(param) {
@@ -85,7 +85,7 @@ function sysmgr_creator_macro(param) {
 }
 
 /**
- * macro renders the trust-state of this weblog
+ * macro renders the trust-state of this site
  */
 
 function sysmgr_trusted_macro(param) {
@@ -101,7 +101,7 @@ function sysmgr_trusted_macro(param) {
 }
 
 /**
- * macro renders the block-state of this weblog
+ * macro renders the block-state of this site
  */
 
 function sysmgr_blocked_macro(param) {
@@ -111,7 +111,7 @@ function sysmgr_blocked_macro(param) {
    if (param.as == "editor") {
       var options = new Array("no","yes");
       var selectedIndex = parseInt(this.blocked,10);
-      res.write(renderDropDownBox("blocked",options,selectedIndex));
+      renderDropDownBox("blocked",options,selectedIndex);
    } else
       res.write(this.isBlocked() ? "yes" : "no");
 }
