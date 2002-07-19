c459331f8d091bfa30ce7604888d4dccb8ec07b9
Robert Gaggl <robert@nomatic.org>
2002-07-19 09:08:41 +0000

    - changed evalFile(): it's not possible anymore to change the alias of a file-object

--- code/File/objectFunctions.js
+++ code/File/objectFunctions.js
@@ -3,25 +3,13 @@
  * @param Obj Object containing form-values
  * @param Obj User-Object modifying file
  * @return Obj Object containing two properties:
- *             - error (boolean): true if error happened, false if everything went fine
+ *             - error (boolean): false
  *             - message (String): containing a message to user
  */
 
 function evalFile(param,modifier) {
-   var result;
-   if (param.alias) {
-      if (param.alias != this.alias) {
-         if (this.site.files.get(param.alias)) {
-            // alias has changed, but is already existing
-            result = getError("nameInUse");
-         } else
-            this.site.files.changeAlias(this,param.alias);
-      }
-      this.description = param.description;
-      this.modifier = modifier;
-      this.modifytime = new Date();
-      result = getConfirm("update");
-   } else
-      result = getError("fileNameMissing");
-   return (result);
+   this.description = param.description;
+   this.modifier = modifier;
+   this.modifytime = new Date();
+   return (getConfirm("update"));
 }
