fdc8fa8c3dab214bd9958287a76cba0b966e2630
Robert Gaggl <robert@nomatic.org>
2002-08-13 20:47:25 +0000

    - removed imageeditor_macro(): this wasn't used since 1.1.1.1 ;-) - added first version of imagelist_macro()

--- code/ImageMgr/macros.js
+++ code/ImageMgr/macros.js
@@ -8,13 +8,29 @@ function images_macro(param) {
    return;
 }
 
-
 /**
- * FIXME!
- *
- * macro renders editor for chosen image
+ * function renders a list of the newest n (default=10) thumbnails
+ * or images
+ * linkto="popup|main"
  */
-
-function imageeditor_macro(param) {
-  return;
+function imagelist_macro(param) {
+   if (!this.size())
+      return;
+   var size = Math.min(param.limit ? param.limit : 5,this.size());
+   var imgcnt = 0;
+   var idx = 0;
+   while (imgcnt < size || imgcnt == this.size()-1) {
+      var imgObj = this.get(idx++);
+      var url = imgObj.getStaticUrl();
+      res.write(param.itemprefix);
+      if (imgObj.thumbnail) {
+         openLink(imgObj.popupUrl());
+         delete(param.linkto);
+         renderImage(imgObj.thumbnail,new Object());
+         closeLink();
+         res.write(param.itemsuffix);
+         imgcnt++;
+      }
+   }
+   return;
 }
