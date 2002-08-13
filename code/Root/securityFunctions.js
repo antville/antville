85d5e09e54e7415c06bf892ebd1d98920dd80099
Robert Gaggl <robert@nomatic.org>
2002-08-13 14:39:31 +0000

    initial check-in

---
+++ code/Root/securityFunctions.js
@@ -0,0 +1,26 @@
+/**
+ * function checks if user is allowed to create a new weblog
+ * @param Obj User-Object
+ */
+
+function isAddSiteDenied(usr) {
+   if (isUserSysAdmin())
+      return null;
+   if (root.sys_limitNewSites == 2 && !isUserSysAdmin())
+      return (getError("siteCreateOnlyAdmins"));
+   else if (root.sys_limitNewSites == 1 && !(isUserSysAdmin() || isUserTrusted()))
+      return (getError("siteCreateNotAllowed"));
+   else if (!root.sys_limitNewSites) {
+      // check if user has been a registered member for long enough
+      var regTime = Math.floor((new Date() - session.user.registered)/86400000);
+      if (regTime < root.sys_minMemberAge)
+         return (getError("siteCreateMemberMinAge",new Array(regTime,root.sys_minMemberAge)));
+      // check if user has to wait some more time before creating a new weblog
+      if (usr.sites.count()) {
+         var lastCreation = Math.floor((new Date() - usr.sites.get(0).createtime)/86400000);
+         if (lastCreation < root.sys_waitAfterNewWeblog)
+            return (getError("siteCreateWait",new Array(root.sys_waitAfterNewWeblog,root.sys_waitAfterNewWeblog - lastCreation)));
+      }
+   }
+   return null;
+}
