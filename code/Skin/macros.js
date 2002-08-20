84d6a35e07edcf8c6421d7d8dbd48f1ace36dad6
Tobi Schäfer <interface@p3k.org>
2002-08-20 13:04:20 +0000

    added comments to help_macro()

--- code/Skin/macros.js
+++ code/Skin/macros.js
@@ -20,29 +20,45 @@ function help_macro(param) {
    var skin = req.data.name;
    var ref = app.data.macros;
    var helpUrl = ref.getProperty("_url");
+
+   // building an object for the various macro types 
    var macros = new Object();
    macros["global"] = ref.getProperty("global.macros");
    var hopjectMacros = ref.getProperty("hopobject.macros");
    macros["root"] = ref.getProperty("root.macros");
+   // no prototype for global macros
    macros[proto] = (proto == "global") ? null : ref.getProperty(proto+".macros");
+   // merge the hopobject's stuff with the prototype's
    macros[proto] += hopjectMacros ? ","+hopjectMacros : "";
    macros.resOrParam = ref.getProperty(proto+".skin."+skin);
-   //macros.response = ref.getProperty(proto+"."+skin+".response");
+
    var re = new RegExp(" *, *");
    re.global = true;
    for (var protoName in macros) {
       if (!macros[protoName])
          continue;
+      // remove spaces before or after a colon
       macros[protoName] = macros[protoName].replace(re, ",");
+      // now transform the string into an array
       var macroNames = macros[protoName].split(",");
       macroNames.sort();
       for (var n in macroNames) {
-         var url;
-         if (protoName != "resOrParam") // && protoName != "res")
-            url = ref.getProperty(protoName+".macro."+macroNames[n]);
+         var url = null;
+         // don't try to get a url for response or param macros
+         if (protoName != "resOrParam") {
+            if (protoName != "global")
+               // first try to get a help url from hopobject
+               url = ref.getProperty("hopobject.macro."+macroNames[n]);
+            if (!url)
+               // if it's not in hopobject maybe it's in the prototype
+               url = ref.getProperty(protoName+".macro."+macroNames[n]);
+         }
+
+         // if a url is available render an html link
          if (url)
             openLink(helpUrl + url);
          res.encode("<% ");
+         // show the prototype's name if not global, response or param
          if (protoName != "global" && protoName != "resOrParam")
             res.write(protoName+".");
          res.write(macroNames[n]);
