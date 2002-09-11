fcc195b7eaf07c08086dad7fe358b26897f3d92e
Tobi Schäfer <interface@p3k.org>
2002-09-11 14:41:23 +0000

    added condition for displaying shortcut in a form or "as is"

--- code/shortcutmgr/macros.js
+++ code/shortcutmgr/macros.js
@@ -2,10 +2,11 @@
  * function renders a list of shortcuts
  */
 
-function shortcutlist_macro() {
-   var param = new Object();
+function shortcutlist_macro(param) {
+   var skin = param.as == "editor" ? "edit" : "main";
+   delete param.as;
    for (var i=0; i<this.size(); i++) {
       param.id = i;
-      this.get(i).renderSkin("main", param);
+      this.get(i).renderSkin(skin, param);
    }
 }
