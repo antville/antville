eabe06d1ef22b60d02bcf891a6dfa506c0a0eee4
Robert Gaggl <robert@nomatic.org>
2002-12-02 16:27:03 +0000

    if root.sys_frontSite is set the site-object is now assigned to res.handlers.site instead to path.site

--- code/HopObject/securityFunctions.js
+++ code/HopObject/securityFunctions.js
@@ -8,27 +8,26 @@ function onRequest() {
    // defining skinpath, membershipLevel
    req.data.memberlevel = 0;
    // if root.sys_frontSite is set and the site is online
-   // we put it into path.site to ensure that the mirrored
+   // we put it into res.handlers.site to ensure that the mirrored
    // weblog works as expected
    if (!path.site && root.sys_frontSite) {
-      var s = root.get(root.sys_frontSite);
-      if (s && s.online)
-         path.site = root.get(root.sys_frontSite);
+      if (root.sys_frontSite.online)
+         res.handlers.site = root.sys_frontSite;
    }
-   if (path.site) {
-      res.skinpath = new Array(path.site.skins);
+   if (res.handlers.site) {
+      res.skinpath = new Array(res.handlers.site.skins);
       if (session.user) {
-         var m = path.site.members.get(session.user.name);
+         var m = res.handlers.site.members.get(session.user.name);
          if (m)
             req.data.memberlevel = m.level;
       }
-      if (path.site.blocked)
+      if (res.handlers.site.blocked)
          res.redirect(root.href("blocked"));
    }
    if (session.user && session.user.blocked) {
       // user was blocked recently, so log out
       session.logout();
       res.message = getMessage("error","accountBlocked");
-      res.redirect(path.site ? path.site.href() : root.href());
+      res.redirect(res.handlers.site ? res.handlers.site.href() : root.href());
    }
 }
