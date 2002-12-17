9dcde6723f658640f4265500a1d0f4a810a3a803
Robert Gaggl <robert@nomatic.org>
2002-12-17 21:54:12 +0000

    added function getNavigationName()

---
+++ code/Topic/objectFunctions.js
@@ -0,0 +1,10 @@
+/**
+ * Return either the title of the story or
+ * the id prefixed with standard display name
+ * to be used in the global linkedpath macro
+ * @see hopobject.getNavigationName()
+ */
+function getNavigationName () {
+   return this.groupname;
+}
+
