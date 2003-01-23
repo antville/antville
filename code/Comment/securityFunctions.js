35f383a97ed8e828c33b0dbb93e76519a67e526e
Robert Gaggl <robert@nomatic.org>
2003-01-23 21:18:56 +0000

    deleted isPostDenied() - using story.isPostDenied() instead

--- code/Comment/securityFunctions.js
+++ code/Comment/securityFunctions.js
@@ -1,19 +1,4 @@
 /**
- * check if user is allowed to post a comment to this story   
- * @param Obj Userobject   
- * @param Int Permission-Level
- * @return String Reason for denial (or null if allowed)   
- */   
- 
-function isPostDenied(usr,level) {
-   if (usr.sysadmin)
-      return null;
-   if (!this.site.online && !level)
-      return "siteNotPublic";
-   return null;   
-}   
-    
-/**
  * check if user is allowed to edit a comment
  * @param Obj Userobject
  * @param Int Permission-Level
