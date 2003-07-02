84c90b20e60f08e427fcae29545964789d9e3b25
Michael Platzer <michael.platzer@knallgrau.at>
2003-07-02 11:27:14 +0000

    implemented the metaWeblog.getCategories and the missing .categories and .flNotOnHomePage-properties to the struct

--- code/MetaWeblogApi/metaWeblogAPI.js
+++ code/MetaWeblogApi/metaWeblogAPI.js
@@ -17,8 +17,10 @@
  *                .dateCreated       Date
  *                .title             String
  *                .description       String containing the body of this entry (Note: this used to be .content in the BLOGGER-API)
+ *                .categories        Array of Strings, containing categories
  *                .link              String
  *                .permaLink         String, equals .link
+ *                .flNotOnHomePage   Boolean, if true then entry just appears in topic
  *                .mt_excerpt        String [MT-API]
  *                .mt_text_more      String [MT-API]
  *                .mt_allow_comments int, 0=no, 1=yes [MT-API]
@@ -49,6 +51,8 @@ function getPost(postid, username, password) {
  *                  .title             String
  *                  .description       String
  *                  .dateCreated       String (ISO.8601)
+ *                  .categories        Array of Strings, containing categories
+ *                  .flNotOnHomePage   Boolean, if true then entry just appears in topic
  *                  .mt_allow_comments int, 0=no, 1=yes [MT-API]
  *                  .mt_allow_pings    int,    currently ignored [MT-API]
  *                  .mt_convert_breaks String, currently ignored [MT-API]
@@ -73,7 +77,14 @@ function newPost (blogid, username, password, content, publish) {
    param.content_text  = content.description;
    if (content.dateCreated) 
      param.createtime = content.dateCreated.format("yyyy-MM-dd HH:mm");
-   param.online = publish ? 2 : 0;
+   if (content.categories && content.categories.length>0)
+      param.topic = content.categories[0];
+   if (publish) 
+      param.online = 2;
+   else if (content.flNotOnHomePage && param.topic) 
+      param.online = 1;
+   else 
+      param.online = 0;
    param.discussions = content.discussions == 0 ? 0 : null;
    var result = blog.stories.evalNewStory(new story(), param, usr);
    if (result.error)
@@ -92,6 +103,8 @@ function newPost (blogid, username, password, content, publish) {
  *                  .title             String
  *                  .description       String
  *                  .dateCreated       String (ISO.8601)
+ *                  .categories        Array of Strings, containing categories
+ *                  .flNotOnHomePage   Boolean, if true then entry just appears in topic
  *                  .mt_allow_comments int, 0=no, 1=yes [MT-API]
  *                  .mt_allow_pings    int,    currently ignored [MT-API]
  *                  .mt_convert_breaks String, currently ignored [MT-API]
@@ -116,8 +129,16 @@ function editPost (postid, username, password, content, publish) {
    param.content_text  = content.description;
    if (content.dateCreated) 
      param.createtime = (content.dateCreated).format("yyyy-MM-dd HH:mm");
-   param.online = publish ? 2 : 0;
+   if (content.categories && content.categories.length>0)
+      param.topic = content.categories[0];
+   if (publish) 
+      param.online = 2;
+   else if (content.flNotOnHomePage && param.topic) 
+      param.online = 1;
+   else 
+      param.online = 0;
    param.discussions = content.mt_allow_comments == 0 ? 0 : null;
+
    var result = entry.evalStory(param, usr);
    return (!result.error);
 }
@@ -168,6 +189,40 @@ function getRecentPosts(blogid, username, password, numberOfPosts) {
 
 
 /**
+ *  metaWeblog.getCategories
+ *  returns a list of categories for a weblog
+ *  @param blogid         String
+ *  @param username       String
+ *  @param password       String
+ *  @return Array of Objects representing Categories
+ *                   with the following properties
+ *                   .description       String
+ *                   .htmlUrl           String
+ *                   .rssUrl            String
+ */
+function getCategories(blogid, username, password) {
+   var usr = root.blogger.getUser(username,password);
+   var blog = root.blogger.getBlog(blogid.toString());
+   if (!blog)
+      throwError ("Couldn't find the blog " + blogid);
+   var level = blog.members.getMembershipLevel(usr);
+   if (blog.isNotPublic(usr, level))
+      trowError("You're not allowed to view the blog " + blogid);
+
+   var arr = blog.topics.list();
+   var topics = new Array();
+   for (var i=0; i<arr.length; i++) {
+     var param = new Object();
+     param.description = arr[i].groupname;
+     param.htmlUrl = arr[i].href();
+     param.rssUrl = null;
+     topics[topics.length] = param;
+   }
+   return (topics);
+}
+
+
+/**
  *  metaWeblog.newMediaObject
  *  uploads a file to the webserver
  *  @param blogid     String
@@ -210,6 +265,8 @@ function convertStoryToStruct (entry) {
    obj.dateCreated = entry.createtime;
    obj.title = entry.getContentPart("title");
    obj.description = entry.getContentPart("text");
+   obj.categories = entry.topic ? new Array(entry.topic) : new Array();
+   obj.flNotOnHomePage = entry.online==1 ? true : false;
    obj.link = entry.href();
    obj.permaLink = obj.link;
    obj.mt_excerpt = null;
