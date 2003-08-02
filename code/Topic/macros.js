139b8cbc00e50a64f60118790f4c62d9690ae917
Robert Gaggl <robert@nomatic.org>
2003-08-02 11:09:31 +0000

    - changed to use AntvilleLib methods - call storymgr.isDenied() instead of doubling the check

--- code/Topic/macros.js
+++ code/Topic/macros.js
@@ -2,19 +2,17 @@
  * Display a link to let the use add a new writeup 
  * to this topic.
  */
-
 function addstory_macro (param) {
-   if ((req.data.memberlevel & MAY_ADD_STORY) == 0)
+   if (path.site.stories.isDenied(session.user, req.data.memberlevel))
       return;
-   var text = param.text;
    param.linkto = "create";
    param.urlparam = "topic=" + this.groupname;
-   openMarkupElement("a",path.site.stories.createLinkParam(param));
-   if (text)
-      res.format(text);
+   Html.openTag("a", path.site.stories.createLinkParam(param));
+   if (param.text)
+      res.format(param.text);
    else
       res.write("add a story to this topic");
-   closeMarkupElement("a");
+   Html.closeTag("a");
 }
 
 /**
@@ -23,7 +21,6 @@ function addstory_macro (param) {
  * but avoiding self-referential backlinks
  * DEPRECATED since the wiki-functionality was dropped!
  */
-
 function relatedtopics_macro (param) {
    return;
    this.related.subnodeRelation = "where TEXT_F_SITE = " + path.site._id + " AND TEXT_TEXT like '%*" + this.groupname + "*%' AND (TEXT_TOPIC is null OR TEXT_TOPIC != '" + this.groupname + "') ORDER BY TEXT_MODIFYTIME desc";
@@ -38,7 +35,6 @@ function relatedtopics_macro (param) {
  * macro writes storylist to response-object
  * kept for backwards-compatibility only
  */
-
 function storylist_macro(param) {
    res.write(res.data.storylist);
    return;
