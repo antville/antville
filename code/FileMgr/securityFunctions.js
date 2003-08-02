627191e07df69e2c368ba10f73af8694691962a7
Robert Gaggl <robert@nomatic.org>
2003-08-02 12:06:45 +0000

    - added checkAccess() that is called by onRequest() to check if a user is allowed to request a certain action - modified isDenied() since site preferences are now xml-encoded

--- code/FileMgr/securityFunctions.js
+++ code/FileMgr/securityFunctions.js
@@ -1,12 +1,26 @@
 /**
- * check if user is allowed to edit images
+ * permission check (called by hopobject.onRequest())
+ * @param String name of action
+ * @param Obj User object
+ * @param Int Membership level
+ * @return Obj Exception object or null
+ */
+function checkAccess(action, usr, level) {
+   checkIfLoggedIn(this.href(req.action));
+   var deny = this.isDenied(session.user, req.data.memberlevel);
+   if (deny)
+      deny.redirectTo = this._parent.href();
+   return deny;
+}
+
+/**
+ * check if user is allowed to edit files
  * @param Obj Userobject
  * @param Int Permission-Level
- * @return String Reason for denial (or null if allowed)
+ * @return Obj Exception or null
  */
-
-function isDenied(usr,level) {
-   if (!this._parent.usercontrib && (level & MAY_ADD_FILE) == 0)
-      return "fileAddDenied";
+function isDenied(usr, level) {
+   if (!this._parent.preferences.getProperty("usercontrib") && (level & MAY_ADD_FILE) == 0)
+      return new Exception("fileAddDenied");
    return null;
 }
--- code/ImageMgr/securityFunctions.js
+++ code/ImageMgr/securityFunctions.js
@@ -1,12 +1,26 @@
 /**
+ * permission check (called by hopobject.onRequest())
+ * @param String name of action
+ * @param Obj User object
+ * @param Int Membership level
+ * @return Obj Exception object or null
+ */
+function checkAccess(action, usr, level) {
+   checkIfLoggedIn(this.href(req.action));
+   var deny = this.isDenied(session.user, req.data.memberlevel);
+   if (deny)
+      deny.redirectTo = getParent().href();
+   return deny;
+}
+
+/**
  * check if user is allowed to edit images
  * @param Obj Userobject
  * @param Int Permission-Level
  * @return String Reason for denial (or null if allowed)
  */
-
-function isDenied(usr,level) {
-   if (!this._parent.usercontrib && (level & MAY_ADD_IMAGE) == 0)
-      return "imageAddDenied";
+function isDenied(usr, level) {
+   if (!this._parent.preferences.getProperty("usercontrib") && (level & MAY_ADD_IMAGE) == 0)
+      return new Exception("imageAddDenied");
    return null;
 }
