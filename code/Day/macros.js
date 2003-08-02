ec570275843e47f856ca4ece6bbc3d93956c9a09
Robert Gaggl <robert@nomatic.org>
2003-08-02 10:47:40 +0000

    - changed to use AntvilleLib - renamed timestamp_macro() to date_macro()

--- code/Day/macros.js
+++ code/Day/macros.js
@@ -13,19 +13,19 @@ function storylist_macro(param) {
  *  Overwrite link macro to use groupname. No fancy options.
  */
 function link_macro () {
-   var attr = new Object();
-   attr.href = this.href();
-   openMarkupElement("a", attr);
-   res.write(this.groupname);
-   closeMarkupElement("a");
+   Html.link(this.href(), this.groupname);
    return;
 }
 
 
 /**
- * return the groupname as formatted timestamp
+ * return the groupname as formatted date
  */
-function timestamp_macro(param) {
-   var ts = parseTimestamp(this.groupname, "yyyyMMdd");
-   return formatTimestamp(ts, param.format);
+function date_macro(param) {
+   var ts = this.groupname.toDate("yyyyMMdd", this._parent.getTimeZone());
+   try {
+      return formatTimestamp(ts, param.format);
+   } catch (err) {
+      return "[invalid format pattern]";
+   }
 }
