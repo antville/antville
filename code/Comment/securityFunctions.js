4c3e6877a7af1383e8cf9b3616a16517296bb50a
Robert Gaggl <robert@nomatic.org>
2003-08-02 12:07:08 +0000

    - added checkAccess() that is called by onRequest() to check if a user is allowed to request a certain action

--- code/Comment/securityFunctions.js
+++ code/Comment/securityFunctions.js
@@ -1,12 +1,44 @@
 /**
+ * permission check (called by hopobject.onRequest())
+ * @param String name of action
+ * @param Obj User object
+ * @param Int Membership level
+ * @return Obj Exception object or null
+ */
+function checkAccess(action, usr, level) {
+   var deny = null;
+   var url = this.story.href();
+   switch (action) {
+      case "edit" :
+         if (!usr && req.data.save)
+            rescueText(req.data);
+         checkIfLoggedIn(this.href(req.action));
+         deny = this.isEditDenied(usr, level);
+         break;
+      case "delete" :
+         checkIfLoggedIn();
+         deny = this.isDeleteDenied(usr, level);
+         break;
+      case "comment" :
+         if (!usr && req.data.save)
+            rescueText(req.data);
+         checkIfLoggedIn(this.href(req.action));
+         deny = this.story.isPostDenied(usr, level);
+         break;
+   }
+   if (deny != null)
+      deny.redirectTo = url;
+   return deny;
+}
+
+/**
  * check if user is allowed to edit a comment
  * @param Obj Userobject
  * @param Int Permission-Level
  * @return String Reason for denial (or null if allowed)
  */
-
-function isEditDenied(usr,level) {
+function isEditDenied(usr, level) {
    if (this.creator != usr && (level & MAY_EDIT_ANYCOMMENT) == 0)
-      return "commentEditDenied";
+      return new Exception("commentEditDenied");
    return null;
 }
--- code/MemberMgr/securityFunctions.js
+++ code/MemberMgr/securityFunctions.js
@@ -1,13 +1,35 @@
 /**
+ * permission check (called by hopobject.onRequest())
+ * @param String name of action
+ * @param Obj User object
+ * @param Int Membership level
+ * @return Obj Exception object or null
+ */
+function checkAccess(action, usr, level) {
+   var deny = null;
+   switch (action) {
+      case "main" :
+         checkIfLoggedIn(this.href(action));
+         deny = this.isEditMembersDenied(usr, level);
+         break;
+      case "subscriptions" :
+         checkIfLoggedIn(this.href(action));
+         break;
+   }
+   if (deny != null)
+      deny.redirectTo = this._parent.href();
+   return deny;
+}
+
+/**
  * check if user is allowed to edit the memberlist of this site
  * @param Obj Userobject
  * @param Int Permission-Level
  * @return String Reason for denial (or null if allowed)
  */
-
-function isEditMembersDenied(usr,level) {
+function isEditMembersDenied(usr, level) {
    if ((level & MAY_EDIT_MEMBERS) == 0)
-      return ("memberEditDenied");
+      return new Exception("memberEditDenied");
    return null;
 }
 
