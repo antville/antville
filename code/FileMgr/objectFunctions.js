aed223bad1e9aabb43671fccfae502e4c07bda2f
Robert Gaggl <robert@nomatic.org>
2003-08-02 11:20:24 +0000

    - changed to use AntvilleLib methods - modified evalFile(): it now throws an Exception object in case something is wrong/missing

--- code/FileMgr/objectFunctions.js
+++ code/FileMgr/objectFunctions.js
@@ -6,52 +6,38 @@
  *             - error (boolean): true if error happened, false if everything went fine
  *             - message (String): containing a message to user
  */
-
-function evalFile(param,creator) {
+function evalFile(param, creator) {
    if (param.uploadError) {
       // looks like the file uploaded has exceeded uploadLimit ...
-      return (getError("fileFileTooBig"));
-   } else if (param.rawfile) {
-      if (param.rawfile.contentLength == 0) {
-         // looks like nothing was uploaded ...
-         return (getError("fileNoUpload"));
-      } else {
-         var newFile = new file();
-         // if no alias given try to determine it
-         if (!param.alias)
-            param.alias = buildAliasFromFile(param.rawfile);
-         else if (!isCleanForName(param.alias))
-            return (getError("noSpecialChars"));
-         // check if alias is already in use
-         if (this.get(param.alias)) {
-            var nr = 1;
-            while (this.get(param.alias + nr))
-               nr++;
-            param.alias = param.alias + nr;
-         }
-         // store properties necessary for file-creation
-         newFile.alias = param.alias;
-         newFile.site = this._parent;
-         newFile.alttext = param.alttext;
-         newFile.name = param.alias;
-         newFile.filesize = param.rawfile.contentLength;
-         newFile.mimetype = param.rawfile.contentType;
-         newFile.description = param.description;
-         newFile.requestcnt = 0;
-         var saveTo = getProperty("filePath") + this._parent.alias + "/";
-         newFile.name = param.rawfile.writeToFile(saveTo,newFile.name);
-         if (newFile.name) {
-            // the file is on disk, so we add the file-object
-            newFile.creator = creator;
-            newFile.createtime = new Date();
-            if (this.add(newFile)) {
-               return (getConfirm("fileCreate",newFile.alias));
-            } else
-               return (getError("fileCreate",newFile.alias));
-         } else
-            return (getError("fileSave"));
-      }
+      throw new Exception("fileFileTooBig");
    }
+   if (!param.rawfile || param.rawfile.contentLength == 0) {
+      // looks like nothing was uploaded ...
+      throw new Exception("fileNoUpload");
+   }
+   var newFile = new file(creator);
+   // if no alias given try to determine it
+   if (!param.alias)
+      newFile.alias = buildAliasFromFile(param.rawfile, this);
+   else {
+      if (!param.alias.isFileName())
+         throw new Exception("noSpecialChars");
+      newFile.alias = buildAlias(param.alias, this);
+   }
+   // store properties necessary for file-creation
+   newFile.alttext = param.alttext;
+   newFile.name = newFile.alias;
+   newFile.filesize = param.rawfile.contentLength;
+   newFile.mimetype = param.rawfile.contentType;
+   newFile.description = param.description;
+   var saveTo = getProperty("filePath") + this._parent.alias + "/";
+   newFile.name = param.rawfile.writeToFile(saveTo, newFile.name);
+   if (!newFile.name)
+      throw new Exception("fileSave");
+   // the file is on disk, so we add the file-object
+   if (!this.add(newFile))
+      throw new Exception("fileCreate", newFile.alias);
+   return new Message("fileCreate", newFile.alias);
 }
 
 
@@ -61,14 +47,13 @@ function evalFile(param,creator) {
  * @return String Message indicating success or failure
  */
 
-function deleteFile(currFile) {
+function deleteFile(fileObj) {
    // first remove the file from disk
-   var f = new File(getProperty("filePath") + currFile.site.alias, currFile.name);
+   var f = FileLib.get(getProperty("filePath") + fileObj.site.alias, fileObj.name);
    f.remove();
-   if (this.remove(currFile))
-      return (getMessage("confirm","fileDelete"));
-   else
-      return (getMessage("error","fileDelete"));
+   if (!this.remove(fileObj))
+      throw new Exception("fileDelete");
+   return new Message("fileDelete");
 }
 
 /**
@@ -76,9 +61,7 @@ function deleteFile(currFile) {
  */
 
 function deleteAll() {
-   for (var i=this.size();i>0;i--) {
-      var f = this.get(i-1);
-      this.deleteFile(f);
-   }
+   for (var i=this.size();i>0;i--)
+      this.deleteFile(this.get(i-1));
    return true;
 }
