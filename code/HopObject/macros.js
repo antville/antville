c352b6d11c0f164a831f8ede8dd1f7dc7ac8796d
Robert Gaggl <robert@nomatic.org>
2003-08-02 10:53:21 +0000

    - removed href_macro() because this is part of AntvilleLib now - removed deprecated input_macro() - use the global one instead - removed check for param.checkdeny in link_macro since i couldn't figure out why it was there

--- code/HopObject/macros.js
+++ code/HopObject/macros.js
@@ -4,7 +4,6 @@
  *                   - suffix
  *                   - name of skin
  */
-
 function skin_macro(param) {
    if (!param.name)
       return;
@@ -15,34 +14,29 @@ function skin_macro(param) {
  * macro creates an html link
  */
 function link_macro(param) {
-   if (param.checkdeny == "true" && this.isDenied(session.user,req.data.memberlevel))
-      return;
-   delete param.checkdeny;
    var content = param.text ? param.text : param.to;
    param = this.createLinkParam(param);
-   openMarkupElement("a", param);
+   Html.openTag("a", param);
    res.write(content);
-   closeMarkupElement("a");
+   Html.closeTag("a");
 }
 
 /**
  * macro renders the time the object was created
  */
-
 function createtime_macro(param) {
-   if (!this.createtime || !param.format)
-      return;
-   res.write(formatTimestamp(this.createtime, param.format));
+   if (this.createtime)
+      res.write(formatTimestamp(this.createtime, param.format));
+   return;
 }
 
 
 /**
  * macro rendering modifytime
  */
-
 function modifytime_macro(param) {
    if (this.modifytime)
-      res.write(formatTimestamp(this.modifytime,param.format));
+      res.write(formatTimestamp(this.modifytime, param.format));
    return;
 }
 
@@ -51,36 +45,14 @@ function modifytime_macro(param) {
  * macro renders the name of the creator of an object
  * either as link or as plain text
  */
-
 function creator_macro(param) {
    if (!this.creator)
       return;
-   if (param.as == "link" && this.creator.url) {
-      openLink(this.creator.url);
-      res.write(this.creator.name);
-      closeLink();
-   } else
+   if (param.as == "link" && this.creator.url)
+      Html.link(this.creator.url, this.creator.name);
+   else
       res.write(this.creator.name);
    return;
-}
-
-/** 
- * Returns a href for an object.
- */
-function href_macro(param) {
-   if (param.action)
-      return this.href(param.action);
-   else
-      return this.href();
-}
-
-/**
- * macro renders a form-input
- * DEPRECATED: just left for backwards-compatibility, use global input_macro() now
- */
-
-function input_macro(param) {
-   input_macro(param);
 }
 
 /**
@@ -89,5 +61,3 @@ function input_macro(param) {
 function url_macro() {
    res.write(this.href());
 }
-
-
