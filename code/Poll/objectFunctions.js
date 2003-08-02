b8c677ae50534c5d7eeaf815c093f950a887db76
Robert Gaggl <robert@nomatic.org>
2003-08-02 11:36:12 +0000

    - changed to use AntvilleLib methods - added constructor function - modified functions to throw an Exception object in case something is wrong - simplified evalPoll() and evalVote()

--- code/Poll/objectFunctions.js
+++ code/Poll/objectFunctions.js
@@ -1,4 +1,15 @@
 /**
+ * constructor function for poll objects
+ */
+function constructor(question, creator) {
+   this.question = question;
+   this.creator = creator;
+   this.closed = 0;
+   this.createtime = this.modifytime = new Date();
+}
+
+
+/**
  * check if poll is ok. if true, save modified poll
  * @param Object the req.data object coming in from the action
  * @param Object the user as creator of the poll modifications
@@ -8,74 +19,55 @@
  *                - url (String): the URL string of the poll
  *                - id (Number): the internal Hop ID of the poll
  */
-
-function evalPoll(param, creator) {
-   var result;
-   var choiceInput = param.choice;
-   if (param.choice_array) {
-      var choiceCnt = 0;
-      for (var i in param.choice_array) {
-         if (param.choice_array[i])
-            choiceCnt++;
-      }
+function evalPoll(question, choices, creator) {
+   if (!question || !choices || choices.length < 2)
+      throw new Exception("pollMissingValues");
+   this.question = question;
+   this.modifytime = new Date();
+   for (var i=this.size(); i>0; i--) {
+      var c = this.get(i-1);
+      this.remove(c);
    }
-   if (param.question && choiceCnt > 1) {
-      this.question = param.question;
-      this.modifytime = new Date();
-      for (var i=this.size(); i>0; i--) {
-         var ch = this.get(i-1);
-         this.remove(ch);
-      }
-      for (var i=0; i<param.choice_array.length; i++) {
-         var title = param.choice_array[i];
-         if (!title)
-            continue;
-         var newChoice = new choice();
-         newChoice.poll = this;
-         newChoice.title = title;
-         newChoice.createtime = new Date();
-         newChoice.modifytime = new Date();
-         this.add(newChoice);
-      }
-      result = getConfirm("pollCreate");
-   } else
-      result = getError("pollMissingValues");
-   return(result);
+   for (var i=0;i<choices.length;i++)
+      this.add(choices[i]);
+   return new Message("pollCreate");
 }
 
 
 /**
  * check if a vote is ok. if true, save modified vote
  * @param Object the req.data object coming in from the action
- * @param Object the user as creator of the poll modifications
+ * @param Object the voting user object
  * @return Object containing the properties
  *                - error (boolean): true if error occured, false otherwise
  *                - message (String): an error or a confirmation message
  *                - url (String): the URL string of the poll
  */
-
 function evalVote(param, usr) {
-	var result;
-	if (param.choice) {
-		var c = this.get(param.choice);
-		var v = session.user ? this.votes.get(session.user.name) : null;
-		if (v) {
-			v.choice = c;
-			v.modifytime = new Date();
-		}
-		else {
-			var v = new vote();
-			v.poll = this;
-			v.choice = c;
-			v.user = session.user;
-			v.username = session.user.name;
-			v.createtime = new Date();
-			v.modifytime = new Date();
-			this.votes.add(v);
-		}
-		result = getConfirm("vote");
+   if (this.isVoteDenied(usr, req.data.memberlevel))
+      throw new Exception("error");
+	if (!param.choice)
+	   throw new Exception("noVote");
+	var c = this.get(param.choice);
+	var v = usr ? this.votes.get(usr.name) : null;
+	if (v) {
+		v.choice = c;
+		v.modifytime = new Date();
 	} else
-		result = getConfirm("noVote");
-	result.url = this.href();
-	return(result);
+		this.votes.add(new vote(c, usr));
+	return new Message("vote");
+}
+
+/**
+ * function loops over the choices of a poll
+ * and removes them
+ */
+function deleteAll() {
+   for (var i=this.size();i>0;i--) {
+      var c = this.get(i-1);
+      c.deleteAll();
+      if (!this.remove(c))
+         throw new Exception("choiceDelete");
+   }
+   return true;
 }
