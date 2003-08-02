ea609ecb7d69e809c2caa2ab2ba049e0f7cb0e9c
Robert Gaggl <robert@nomatic.org>
2003-08-02 11:52:53 +0000

    - removed method sendConfirmationMail - sortSubscriptions is now failsafe, although it shouldn't happen anymore that a membership object still exists for a removed site

--- code/User/objectFunctions.js
+++ code/User/objectFunctions.js
@@ -1,27 +1,11 @@
 /**
- * send a mail to confirm registration
- * @param String email-address used as sender-address
- */
-
-function sendConfirmationMail(sender) {
-   var mail = new Mail();
-   mail.setFrom(sender);
-   mail.addTo(this.email);
-   mail.setSubject(getMessage("mailsubject","registration",root.getSysTitle()));
-   var mailParam = new Object();
-   mailParam.name = this.name;
-   mailParam.password = this.password;
-	mail.setText(this.renderSkinAsString("mailbody",mailParam));
-	mail.send();
-}
-
-
-/**
  * function for sorting member-objects by the lastupdate-timestamp
  * of the according site
  */
 
-function sortSubscriptions(s1,s2) {
+function sortSubscriptions(s1, s2) {
+   if (!s1.site || !s2.site)
+      return 0;
    if (s1.site.lastupdate < s2.site.lastupdate)
       return 1;
    else if (s1.site.lastupdate > s2.site.lastupdate)
