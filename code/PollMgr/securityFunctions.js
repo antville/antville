a48c62511c00858edd60fe811697983b0a8f130a
Robert Gaggl <robert@nomatic.org>
2003-08-02 12:09:51 +0000

    - added checkAccess() that is called by onRequest() to check if a user is allowed to request a certain action - site preferences are xml encoded now - modified permission check methods: they now return an Exception object in case a certain action is denied

--- code/PollMgr/securityFunctions.js
+++ code/PollMgr/securityFunctions.js
@@ -1,13 +1,38 @@
 /**
+ * permission check (called by hopobject.onRequest())
+ * @param String name of action
+ * @param Obj User object
+ * @param Int Membership level
+ * @return Obj Exception object or null
+ */
+function checkAccess(action, usr, level) {
+   var deny = null;
+   var url = this._parent.href();
+   switch (action) {
+      case "main" :
+         checkIfLoggedIn(this.href(req.action));
+         deny = this.isDenied(usr, level);
+         break;
+      case "create" :
+         checkIfLoggedIn();
+         deny = this.isDenied(usr, level);
+         url = this.href();
+         break;
+   }
+   if (deny != null)
+      deny.redirectTo = url;
+   return deny;
+}
+
+/**
  * function checks if user is allowed to view the storylist
  * of this site
  * @param Obj Userobject
  * @param Int Permission-Level
  * @return String Reason for denial (or null if allowed)
  */
-
-function isDenied(usr,level) {
-   if (!this._parent.usercontrib && (level & MAY_ADD_STORY) == 0)
-      return "pollAddDenied";
+function isDenied(usr, level) {
+   if (!this._parent.preferences.getProperty("usercontrib") && (level & MAY_ADD_STORY) == 0)
+      return new Exception("pollAddDenied");
    return null;
 }
