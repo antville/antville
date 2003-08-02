6ec7bd118a388b654c2d0639e5dcba06f8e90628
Robert Gaggl <robert@nomatic.org>
2003-08-02 11:54:34 +0000

    changed from path.site to this._parent where possible

--- code/Day/renderFunctions.js
+++ code/Day/renderFunctions.js
@@ -8,23 +8,23 @@
  */
 
 function renderStorylist() {
-   var dayIdx = path.site.contains(this);
+   var dayIdx = this._parent.contains(this);
    if (dayIdx > 0) {
       var sp = new Object();
-      sp.url = path.site.get(path.site.contains(this)-1).href();
+      sp.url = this._parent.get(dayIdx - 1).href();
       sp.text = "newer stories";
-      res.data.prevpage = renderSkinAsString("prevpagelink",sp);
+      res.data.prevpage = renderSkinAsString("prevpagelink", sp);
    }
    var storylist = new java.lang.StringBuffer();
    storylist.append(this.get(0).renderSkinAsString("dayheader"));
    for (var i=0;i<this.size();i++)
       storylist.append(this.get(i).renderSkinAsString("preview"));
    // assigning link to previous page to res.data.prevpage
-   if (dayIdx < path.site.size()-1) {
+   if (dayIdx < this._parent.size()-1) {
       var sp = new Object();
-      sp.url = path.site.get(path.site.contains(this)+1).href();
+      sp.url = this._parent.get(dayIdx + 1).href();
       sp.text = "older stories";
-      res.data.nextpage = renderSkinAsString("nextpagelink",sp);
+      res.data.nextpage = renderSkinAsString("nextpagelink", sp);
    }
    res.data.storylist = storylist.toString();
    return;
