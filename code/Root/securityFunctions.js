049d34d8ec67b1f85590d87a17b66809d5759467
Robert Gaggl <robert@nomatic.org>
2003-09-07 21:00:02 +0000

    - rewrote permission framework: renamed isXxxDenied() to checkXxx(), all check-methods now throw a DenyException object instead of returning an Exception object - use constant ONEDAY

--- code/Root/securityFunctions.js
+++ code/Root/securityFunctions.js
@@ -6,23 +6,25 @@
  * @return Obj Exception object or null
  */
 function checkAccess(action, usr, level) {
-   var deny = null;
-   switch (action) {
-      case "new" :
-         checkIfLoggedIn(this.href("new"));
-         deny = this.isAddSiteDenied(usr, level);
-         break;
+   try {
+      switch (action) {
+         case "new" :
+            checkIfLoggedIn(this.href("new"));
+            this.checkAdd(usr, level);
+            break;
+      }
+   } catch (deny) {
+      res.message = deny.toString();
+      res.redirect(this.href());
    }
-   if (deny != null)
-      deny.redirectTo = this.href();
-   return deny;
+   return;
 }
 
 /**
  * function checks if user is allowed to create a new weblog
  * @param Obj User-Object
  */
-function isAddSiteDenied(usr) {
+function checkAdd(usr) {
    // sysAdmins aren't restricted
    if (session.user.sysadmin)
       return null;
@@ -30,27 +32,27 @@ function isAddSiteDenied(usr) {
    switch (root.sys_limitNewSites) {
       case 2:
          if (!usr.sysadmin)
-            return new Exception("siteCreateOnlyAdmins");
+            throw new DenyException("siteCreateOnlyAdmins");
       case 1:
          if (!usr.trusted)
-            return new Exception("siteCreateNotAllowed");
+            throw new DenyException("siteCreateNotAllowed");
       default:
          if (root.sys_minMemberAge) {
             // check if user has been a registered member for long enough
-            var regTime = Math.floor((new Date() - session.user.registered)/86400000);
+            var regTime = Math.floor((new Date() - session.user.registered)/ONEDAY);
             if (regTime < root.sys_minMemberAge)
-               return new Exception("siteCreateMinMemberAge", [regTime, root.sys_minMemberAge]);
+               throw new DenyException("siteCreateMinMemberAge", [regTime, root.sys_minMemberAge]);
          } else if (root.sys_minMemberSince) {
             // check if user has registered before the defined timestamp
             if (session.user.registered > root.sys_minMemberSince)
-               return new Exception("siteCreateMinMemberSince", formatTimestamp(root.sys_minMemberSince));
+               throw new DenyException("siteCreateMinMemberSince", formatTimestamp(root.sys_minMemberSince));
          }
          if (usr.sites.count()) {
             // check if user has to wait some more time before creating a new weblog
-            var lastCreation = Math.floor((new Date() - usr.sites.get(0).createtime)/86400000);
+            var lastCreation = Math.floor((new Date() - usr.sites.get(0).createtime)/ONEDAY);
             if (lastCreation < root.sys_waitAfterNewSite)
-               return new Exception("siteCreateWait", [root.sys_waitAfterNewSite, root.sys_waitAfterNewSite - lastCreation]);
+               throw new DenyException("siteCreateWait", [root.sys_waitAfterNewSite, root.sys_waitAfterNewSite - lastCreation]);
          }
    }
-   return null;
+   return;
 }
