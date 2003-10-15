6d9851250b368167ba8f616ba72eca5c7dd180e2
Robert Gaggl <robert@nomatic.org>
2003-10-15 19:59:26 +0000

    removed method renderPollList() which is replaced by global renderList(), added method renderManagerView() that is passed as argument to global renderList() (this method does the actual rendering of an item in the list)

--- code/PollMgr/renderFunctions.js
+++ code/PollMgr/renderFunctions.js
@@ -1,54 +1,11 @@
 /**
- * function renders the list of polls and assigns
- * the rendered list to res.data.storylist
- * scrollnavigation-links to previous and next page(s) are also
- * assigned to res.data (res.data.prevpage, res.data.nextpage)
- * using this separate renderFunction instead of doing the stuff
- * in storylist_macro() was necessary for completely independent
- * placement of the prevpage- and nextpage-links
- * @param Int Index-position to start with
- * @param String String indicating what kind of polls to show
+ * check if a story is online or offline
+ * and render the listitem view accordingly
+ * @param Object poll object to render
  */
-
-function renderPollList(idx, show) {
-   var size = this.size();
-   if (isNaN (idx)|| idx > size-1)
-      idx = 0;
-   var cnt = 0;
-   var max = Math.min (10, size);
-
-   if (idx > 0) {
-      var sp = new Object();
-      sp.url = this.href() + "?start=" + Math.max(0, idx-10) + (show ? "&show=" + show : "");
-      sp.text = "previous polls";
-      res.data.prevpage = renderSkinAsString("prevpagelink", sp);
-   }
-
-   var plist = new java.lang.StringBuffer();
-   var sp = new Object();
-   while (cnt < max && idx < size) {
-      var render = true;
-      var st = this.get(idx);
-      if (show == "open" && st.closed)
-         render = false;
-      else if (show == "mypolls" && st.creator != session.user)
-         render = false;
-      if (render) {
-         sp.poll = st.renderSkinAsString("listitem");
-         if (st.closed)
-            plist.append(this.renderSkinAsString("closedpoll", sp));
-         else
-            plist.append(this.renderSkinAsString("openpoll", sp));
-         cnt++;
-      }
-      idx++;
-   }
-   if (idx < size) {
-      var sp = new Object();
-      sp.url = this.href() + "?start=" + idx + (show ? "&show=" + show : "");
-      sp.text = "more polls";
-      res.data.nextpage = renderSkinAsString("nextpagelink", sp);
-   }
-   res.data.pollList = plist.toString();
-   return;
+function renderManagerView(poll) {
+   var sp = {poll: poll.renderSkinAsString("listitem")};
+   if (poll.closed == 1)
+      return this.renderSkinAsString("closedpoll", sp);
+   return this.renderSkinAsString("openpoll", sp);
 }
