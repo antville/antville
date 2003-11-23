466a517fd2e3556e1b414dab955b9ebd6b756821
Robert Gaggl <robert@nomatic.org>
2003-11-23 18:23:45 +0000

    - removed skinsets_macro() since there are no skinsets anymore (but layouts) - moved skinstatus_macro() from prototype skin to skinmgr - removed skinsetchooser_macro() since there are no skinsets anymore (but layouts) - moved help_macro() from prototype skin to skinmgr

--- code/SkinMgr/macros.js
+++ code/SkinMgr/macros.js
@@ -1,26 +1,82 @@
 /**
- * macro renders a list of skinsets defined for this site
+ * macro checks if the skin was modified or
+ * is the default-skin
  */
-
-function skinsets_macro(param) {
-   var l = this.size();
-   if (l == 0) {
-      return "- no skinsets -";
+function skinstatus_macro(param) {
+   if (!param.proto || !param.name)
+      return;
+   if (this[param.proto] && this[param.proto][param.name]) {
+      var s = this[param.proto][param.name];
+      res.write("customized by " + s.creator.name);
+      res.write("&nbsp;...&nbsp;");
+      Html.openLink(this.href("diff")+"?proto="+param.proto+"&name="+param.name);
+      res.write("diff");
+      Html.closeLink();
+      res.write("&nbsp;...&nbsp;");
+      Html.openLink(s.href("delete"));
+      res.write("reset");
+      Html.closeLink();
    } else {
-      this.renderSkin("skinsets");
+      res.write("not customized");
    }
 }
 
 /**
- * macro renders a form for creating a new skinset
+ * macro showing the most important macros
+ * available for a specific skin
  */
 
-function skinsetchooser_macro(param) {
-   var sets = new Array(["", "default skinset"]);
-   var l = this.size();
-   for (var i=0; i<l; i++) {
-      var sks = this.get(i);
-      sets[sets.length] = [sks._id, sks.name];
+function help_macro(param) {
+   var proto = req.data.proto;
+   var skin = req.data.name;
+   var ref = app.data.macros;
+   var helpUrl = ref.getProperty("_url");
+
+   // building an object for the various macro types
+   var macros = new Object();
+   macros["global"] = ref.getProperty("global.macros");
+   var hopjectMacros = ref.getProperty("hopobject.macros");
+   macros["root"] = ref.getProperty("root.macros");
+   // no prototype for global macros
+   macros[proto] = (proto == "global") ? null : ref.getProperty(proto + ".macros");
+   // merge the hopobject's stuff with the prototype's
+   macros[proto] += hopjectMacros ? "," + hopjectMacros : "";
+   macros.resOrParam = ref.getProperty(proto + ".skin." + skin);
+
+   var re = new RegExp(" *, *", "g");
+   for (var protoName in macros) {
+      if (!macros[protoName])
+         continue;
+      // remove spaces before or after a colon
+      macros[protoName] = macros[protoName].replace(re, ",");
+      // now transform the string into an array
+      var macroNames = macros[protoName].split(",");
+      macroNames.sort();
+      for (var n in macroNames) {
+         var url = null;
+         // don't try to get a url for response or param macros
+         if (protoName != "resOrParam") {
+            if (protoName != "global")
+               // first try to get a help url from hopobject
+               url = ref.getProperty("hopobject.macro."+macroNames[n]);
+            if (!url)
+               // if it's not in hopobject maybe it's in the prototype
+               url = ref.getProperty(protoName+".macro."+macroNames[n]);
+         }
+
+         // if a url is available render an html link
+         if (url)
+            Html.openLink(helpUrl + url);
+         res.encode("<% ");
+         // show the prototype's name if not global, response or param
+         if (protoName != "global" && protoName != "resOrParam")
+            res.write(protoName+".");
+         res.write(macroNames[n]);
+         res.encode(" %>");
+         if (url)
+            Html.closeLink();
+         Html.tag("br");
+      }
+      Html.tag("br");
    }
-   Html.dropDown("skinset", sets, param.selected, "Choose Skinset");
 }
