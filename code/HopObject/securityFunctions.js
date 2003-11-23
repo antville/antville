898404a923403f6c25ac99eba1a88695e6f9851d
Robert Gaggl <robert@nomatic.org>
2003-11-23 18:57:37 +0000

    modified onRequest(): - assign the currently active layout to res.handlers.layout, also added check if user has a layout object in the session (for testdriving it) - define the skinpath according to the layout object - only call checkAccess if the user isn't a sysadmin (which means that sysadmins are basically allowed to do anything)

--- code/HopObject/securityFunctions.js
+++ code/HopObject/securityFunctions.js
@@ -16,40 +16,38 @@ function onRequest() {
          res.redirect(root.href("blocked"));
       if (session.user)
          req.data.memberlevel = res.handlers.site.members.getMembershipLevel(session.user);
-      // check if the site has a skinset
-      var skinsetId = res.handlers.site.preferences.getProperty("skinset");
-      if (skinsetId) {
-         var skinset = res.handlers.site.skins.get(skinsetId);
-         var skinpath = [];
-         // loop through skinset parent chain, adding each skinset to the skinpath
-         while (skinset) {
-            skinpath[skinpath.length] = skinset;
-            skinset = skinset.parent;
-         }
-         res.skinpath = skinpath;
-      }
-      // set a context handler that contains the context title
-      res.handlers.context = {
-         title: "site "+res.handlers.site.title
-      };
+      // set a handler that contains the context
+      res.handlers.context = res.handlers.site;
    } else {
-      // set a context handler that contains the context title
-      res.handlers.context = {
-         title: root.sys_title 
-      };
+      // set a handler that contains the context
+      res.handlers.context = root;
    }
-   if (session.data.skinset) {
-      // test drive a skinset
-      session.data.skinset.testdrive();
+
+   if (session.data.layout) {
+      // test drive a layout
+      res.handlers.layout = session.data.layout;
+      res.message = session.data.layout.renderSkinAsString("testdrive");
+   } else {
+      // define layout handler
+      res.handlers.layout = res.handlers.context.getLayout();
    }
+
+   // set skinpath according to res.handlers.layout
+   var sp = [res.handlers.layout.skins];
+   if (res.handlers.layout.parent)
+      sp.push(res.handlers.layout.parent.skins);
+   res.skinpath = sp;
+
    if (session.user && session.user.blocked) {
       // user was blocked recently, so log out
       session.logout();
       res.message = new Exception("accountBlocked");
-      res.redirect(res.handlers.site ? res.handlers.site.href() : root.href());
+      res.redirect(res.handlers.context.href());
    }
-   // check access
-   this.checkAccess(req.action, session.user, req.data.memberlevel);
+   // check access, but only if user is *not* a sysadmin
+   // sysadmins are allowed to to everything
+   if (!session.user || !session.user.sysadmin)
+      this.checkAccess(req.action, session.user, req.data.memberlevel);
    return;
 }
 
