22df53c7958cdb7a5ed33262f2d4f3ce58a44fe3
Robert Gaggl <robert@nomatic.org>
2003-11-23 18:48:07 +0000

    - removed methods isDefaultSkinset(), setDefaultSkinset(), testdriveSkinset(), deleteSkinset() since there are no skinsets anymore - removed unused method getNavigationName() - added methods saveSkin(), deleteSkin(), deleteAll() - added getSkinSource() which retrieves either the source of the own skin or the one of the parent layout's skin - added dumpToZip() which dumps the skin source to a Zip object passed as argument - added evalImport() that handles the import of a skinset (based on an uploaded zip file)

--- code/SkinMgr/objectFunctions.js
+++ code/SkinMgr/objectFunctions.js
@@ -1,36 +1,21 @@
 /**
- * Is this skinset the default in the current context?
+ * function stores skin
+ * @param Obj object containing submitted form values (req.data)
+ * @param Obj User object
  */
-function isDefaultSkinset() {
-   if (path.site && path.site.preferences.getProperty("skinset") == this._id)
-      return true;
-   if (!path.site && root.sys_skinset == this._id)
-      return true;
-   return false;
-}
-
-/**
- * Set this skinset to the default in the current context.
- */
-function setDefaultSkinset(skinsetId) {
-   if (path.site && path.site.preferences.getProperty("skinset") != skinsetId)
-      path.site.preferences.setProperty("skinset", skinsetId);
-   else if (!path.site && root.sys_skinset != skinsetId)
-      root.sys_skinset = skinsetId;
-}
-
-/**
- *  Testdrive a skinset. Called by hopobject.onRequest().
- *  FIXME: Currently not used! Should it be?
- */
-function testdriveSkinset() {
-   if (session.data.testdriveSkinset) {
-      var set = this.get(session.data.testdriveSkinset);
-      if (set) {
-         res.skinpath = new Array(set);
-         res.message = set.renderSkinAsString("testdriveMessage");
-      }
+function saveSkin(param, usr) {
+   if (!param.proto || !param.name)
+      throw new Exception("skinUpdate");
+   if (this[param.proto] && this[param.proto][param.name]) {
+      var s = this[param.proto][param.name];
+      s.modifytime = new Date();
+      s.modifier = usr;
+   } else {
+      var s = new skin(this._parent, param.proto, param.name, usr);
+      this.add(s);
    }
+   s.skin = param.skin;
+   return new Message("update");
 }
 
 /**
@@ -38,29 +23,80 @@ function testdriveSkinset() {
  * @param Obj Skin-HopObject to delete
  * @return String Message indicating success of failure
  */
-
-function deleteSkinset(set) {
-   var l = set.size();
-   for (var i=l-1; i>=0; i--) {
-      var group = set.get(i);
-      var m = group.size();
-      for (var j=m-1; j>=0; j--) {
-          set[i][j].remove(); 
-      }
+function deleteSkin(s) {
+   try {
+      this.get(s.proto).remove(s);
+      return new Message("skinDelete");
+   } catch (err) {
+      throw new Exception("skinDelete");
    }
-   if (set.remove())
-      return (getMessage("confirm","skinDelete"));
-   else
-      return (getMessage("error","skinDelete"));
+   return true;
 }
-
 
 /**
- * Return the name of the skin manager
- * to be used in the global linkedpath macro
- * @see hopobject.getNavigationName()
+ * delete all skins belonging to this manager
  */
-function getNavigationName () {
-   return (DISPLAY["skinmgr"]);
+function deleteAll() {
+   for (var i=this.size();i>0;i--) {
+      var proto = this.get(i-1);
+      for (var j=proto.size();j>0;j--)
+         this.deleteSkin(proto.get(j-1));
+   }
+   return true;
 }
 
+/**
+ * return the source of a skin
+ * @param String name of the prototype
+ * @param String name of the skin
+ * @return String source of the skin
+ */
+function getSkinSource(proto, name) {
+   var sp = [this];
+   if (this._parent.parent)
+      sp.push(this._parent.parent.skins);
+   var s = app.__app__.getSkin(proto, name, sp);
+   return (s ? s.source : null);
+}
+
+/**
+ * dump all skins of this skinmgr
+ * @param Object Zip object to add the skins to
+ */
+function dumpToZip(z) {
+   var skins = app.skinfiles;
+   for (var protoName in skins) {
+      var proto = app.skinfiles[protoName];
+      for (var skinName in proto) {
+         var s = proto[skinName];
+         var source = this.getSkinSource(protoName, skinName);
+         if (source) {
+            var buf = new java.lang.String(source).getBytes();
+            z.addData(buf, "skins/" + protoName + "/" + skinName);
+         }
+      }
+   }
+   return;
+}
+
+/**
+ * create the skins of an imported layout
+ * @param Object JS object tree containing the skins data
+ */
+function evalImport(data) {
+   var proto;
+   var skinParam;
+   var buf;
+   for (var protoName in data) {
+      proto = data[protoName];
+      for (var skinName in proto) {
+         skinParam = proto[skinName];
+         // FIXME: replace session.user with a more intelligent solution ...
+         var s = new skin(this._parent, protoName, skinName, session.user);
+         buf = data[protoName][skinName].data;
+         s.skin = new java.lang.String(buf, 0, buf.length);
+         this.add(s);
+      }
+   }
+   return true;
+}
