d922efe115fe0121bbfaf6e38f65429a9a3a91fd
Robert Gaggl <robert@nomatic.org>
2003-12-08 22:34:28 +0000

    - simplyfied renderManagerView() - added method renderParentLayoutChooser()

--- code/LayoutMgr/renderFunctions.js
+++ code/LayoutMgr/renderFunctions.js
@@ -1,11 +1,28 @@
 /**
  * check if a layout is default layout or not
- * and render the listitem view accordingly
+ * and render the mgrlistitem view accordingly
  * @param Object layout object to render
  */
 function renderManagerView(layout) {
-   var sp = {layout: layout.renderSkinAsString("listitem")};
-   if (layout.isDefaultLayout())
-      return this.renderSkinAsString("activelayout", sp);
-   return this.renderSkinAsString("inactivelayout", sp);
+   var sp = {layout: layout.renderSkinAsString("mgrlistitem")};
+   this.renderSkin(layout.isDefaultLayout() ? "activelayout" : "inactivelayout", sp);
+   return;
+}
+
+/**
+ * render a dropdown containing shareable system layouts
+ * @param Object current layout
+ */
+function renderParentLayoutChooser(selLayout) {
+   var options = [];
+   var size = root.layouts.shareable.size();
+   for (var i=0;i<size;i++) {
+      var l = root.layouts.shareable.get(i);
+      options.push({value: l.alias, display: l.title});
+   }
+   var selected = null;
+   if (selLayout && selLayout.parent)
+      selected = selLayout.parent.alias;
+   Html.dropDown("layout", options, selected, "--- choose a basis layout ---");
+   return;
 }
