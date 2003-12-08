d65d2f84741922844bac5f0a4b117e94f9fff0e5
Robert Gaggl <robert@nomatic.org>
2003-12-08 21:53:57 +0000

    modified constructor: by default property custom is 0

--- code/Skin/objectFunctions.js
+++ code/Skin/objectFunctions.js
@@ -5,6 +5,7 @@ function constructor(layout, proto, name, creator) {
    this.layout = layout;
    this.proto = proto;
    this.name = name;
+   this.custom = 0;
    this.creator = this.modifier = creator;
    this.createtime = new Date();
    this.modifytime = new Date();
