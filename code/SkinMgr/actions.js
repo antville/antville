cc48bbe2fb4f158907bd7f068219dea567b4069d
Robert Gaggl <robert@nomatic.org>
2003-12-08 20:06:50 +0000

    - moved diff_action to prototype skin - added custom_action (for creating custom skins) - added modified_action (for listing only modified skins) - various minor changes

--- code/SkinMgr/actions.js
+++ code/SkinMgr/actions.js
@@ -1,70 +1,31 @@
 /**
  * main action
  */
-
 function main_action() {
-   res.data.title = "Skins of " + res.handlers.layout.title;
+   res.data.title = "Skins of " + this._parent.title;
+   res.data.list = this.renderTree(req.data);
    res.data.body = this.renderSkinAsString("main");
    res.handlers.context.renderSkin("page");
 }
 
 /**
- * action rendering the differences between the original skin
- * and the modified one
+ * list only modified skins
  */
-function diff_action() {
-   if (!req.data.proto || !req.data.name ||
-       !this[req.data.proto] || !this[req.data.proto][req.data.name] ||
-       !app.skinfiles[req.data.proto])
-   {
-      res.message = new Exception("skinDiff");
-      res.redirect(this.href());
-   }
+function modified_action() {
+   res.data.title = "Skins of " + this._parent.title;
+   res.data.list = this.renderList(this.modified, req.action);
+   res.data.body = this.renderSkinAsString("main");
+   res.handlers.context.renderSkin("page");
+}
 
-   // get the modified and original skins
-   var modifiedSkin = this[req.data.proto][req.data.name].skin;
-   var sp = [];
-   if (this._parent.parent)
-      sp.push(this._parent.parent.skins);
-   var s = app.__app__.getSkin(req.data.proto, req.data.name, sp);
-   var originalSkin = s.source;
-
-   var buf = new java.lang.StringBuffer();
-   if (originalSkin == null || modifiedSkin == null) {
-      buf.append("Invalid Parameters. No Diff.");
-   } else {
-      buf.append("<h3>Diffs for "+req.data.proto+"/"+req.data.name+"</h3>");
-      var diff = originalSkin.diff(modifiedSkin);
-      if (!diff) {
-         buf.append("No differences were found");
-      } else {
-         // print a short explanation of the output format
-         buf.append("<span style=\"background: #FF3333\">&nbsp;&nbsp;&nbsp;&nbsp;</span> ");
-         buf.append(" Lines removed from original skin<br />");
-         buf.append("<span style=\"background: #33CC33\">&nbsp;&nbsp;&nbsp;&nbsp;</span> ");
-         buf.append(" Lines added to modified skin<br />");
-
-         buf.append("<pre>");
-         for (var i in diff) {
-            var line = diff[i];
-            if (line.deleted) {
-               for (var j=0;j<line.deleted.length;j++)
-                  buf.append((line.num + j) + " DEL <span style=\"background: #FF3333\">" + encode(line.deleted[j]) + "</span>\r\n");
-            }
-            if (line.inserted) {
-               for (var j=0;j<line.inserted.length;j++)
-                  buf.append((line.num + j) + " INS <span style=\"background: #33CC33\">" + encode(line.inserted[j]) + "</span>\r\n");
-            }
-            if (line.value) {
-               buf.append(line.num + "     <span>" + encode(line.value) + "</span>\r\n");
-            }
-         }
-         buf.append("</pre>");
-      }
-   }
-   res.data.title = "Diffs for " + req.data.proto + "/" + req.data.name + ".skin of " + this._parent.title;
-   res.data.body = buf.toString();
-   this.renderSkin("page");
+/**
+ * list only custom skins
+ */
+function custom_action() {
+   res.data.title = "Custom skins of " + this._parent.title;
+   res.data.list = this.renderList(this.custom, req.action);
+   res.data.body = this.renderSkinAsString("main");
+   res.handlers.context.renderSkin("page");
 }
 
 /**
@@ -75,6 +36,7 @@ function diff_action() {
  */
 function safe_action() {
    res.data.title = this._parent.title;
+   res.data.list = this.renderList(this.modified);
    res.data.body = this.renderSkinAsString("main");
    this.renderSkin("page");
 }
@@ -84,20 +46,53 @@ function safe_action() {
  */
 function edit_action() {
    if (req.data.cancel)
-      res.redirect(this.href() + "#" + req.data.proto + req.data.name);
-   else if (req.data.save) {
+      res.redirect(this.href(req.data.action) + "?skinset=" + req.data.skinset + "#" + req.data.key);
+   else if (req.data.save || req.data.close) {
       try {
          res.message = this.saveSkin(req.data, session.user);
-         res.redirect(this.href() + "#" + req.data.proto + req.data.name);
+         if (req.data.close)
+            res.redirect(this.href(req.data.action) + "?skinset=" + req.data.skinset + "#" + req.data.key);
+         res.redirect(this.href(req.action) + "?key=" + req.data.key + "&skinset=" + req.data.skinset + "#" + req.data.skinset);
       } catch (err) {
          res.message = err.toString();
       }
    }
 
-   res.data.skin = this.getSkinSource(req.data.proto, req.data.name);
+   if (!req.data.key)
+      res.redirect(this.href());
+   var sp = new Object();
+   var desc = this.getSkinDescription("skin", req.data.key);
+   sp.title = desc[0];
+   sp.text = desc[1] ? desc[1] : " (custom skin)";
+   var splitKey = req.data.key.split(".");
+   sp.skin = this.getSkinSource(splitKey[0], splitKey[1]);
+   sp.action = req.data.action;
+
    res.data.action = this.href(req.action);
-   res.data.title = req.data.proto + "/" + req.data.name + ".skin of " + res.handlers.layout.title;
-   res.data.body = this.renderSkinAsString("edit");
+   res.data.title = splitKey[0] + "/" + splitKey[1] + ".skin of " + this._parent.title;
+   res.data.body = this.renderSkinAsString("edit", sp);
    this.renderSkin("page");
 }
 
+/**
+ * action for creating a custom skin
+ */
+function create_action() {
+   if (req.data.cancel)
+      res.redirect(this.href());
+   else if (req.data.save) {
+      try {
+         var result = this.evalCustomSkin(req.data, session.user);
+         res.message = result.toString();
+         if (!result.error)
+            res.redirect(this.href("edit") + "?key=" + req.data.prototype + "." + req.data.name);
+      } catch (err) {
+         res.message = err.toString();
+      }
+   }
+
+   res.data.action = this.href(req.action);
+   res.data.title = "Create a custom skin for " + this._parent.title;
+   res.data.body = this.renderSkinAsString("new");
+   res.handlers.context.renderSkin("page");
+}
