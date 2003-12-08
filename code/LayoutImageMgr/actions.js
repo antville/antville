237c5b61cbea7df836ff3195b355e6da2c2962d2
Robert Gaggl <robert@nomatic.org>
2003-12-08 19:57:25 +0000

    - renamed parentimages_action to parent_action - preview.skin of image was renamed to mgrlistitem.skin

--- code/LayoutImageMgr/actions.js
+++ code/LayoutImageMgr/actions.js
@@ -1,12 +1,12 @@
 /**
  * display the images of the parent layout
  */
-function parentimages_action() {
+function parent_action() {
    if (!this._parent.parent) {
       res.message = new Exception("layoutNoParent");
       res.redirect(this.href());
    }
-   res.data.imagelist = renderList(this._parent.parent.images, "preview", 10, req.data.page);
+   res.data.imagelist = renderList(this._parent.parent.images, "mgrlistitem", 10, req.data.page);
    res.data.pagenavigation = renderPageNavigation(this._parent.parent.images, this.href(req.action), 10, req.data.page);
    res.data.title = "Images of layout '" + this._parent.parent.title + "'";
    res.data.body = this.renderSkinAsString("main");
