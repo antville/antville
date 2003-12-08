450940d8cf4608c843a04b98275da5f483504dad
Robert Gaggl <robert@nomatic.org>
2003-12-08 21:07:30 +0000

    removed deprecated relatedtopics_macro (it wasn't used anymore)

--- code/Topic/macros.js
+++ code/Topic/macros.js
@@ -40,23 +40,6 @@ function addimage_macro (param) {
    return;
 }
 
-
-/**
- * Get related topics, i.e. topics that contain stories that
- * link back to this topic.
- * but avoiding self-referential backlinks
- * DEPRECATED since the wiki-functionality was dropped!
- */
-function relatedtopics_macro (param) {
-   return;
-   this.related.subnodeRelation = "where TEXT_F_SITE = " + path.site._id + " AND TEXT_TEXT like '%*" + this.groupname + "*%' AND (TEXT_TOPIC is null OR TEXT_TOPIC != '" + this.groupname + "') ORDER BY TEXT_MODIFYTIME desc";
-   var l = this.related.size();
-   if (l == 0)
-      return;
-   for (var i=0; i<l; i++)
-      this.related.get(i).renderSkin("relatedLink");
-}
-
 /**
  * macro writes storylist to response-object
  * kept for backwards-compatibility only
