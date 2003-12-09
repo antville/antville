c7a39689ba45f3f1b25fe4ae7bf2bac12835d3fb
Robert Gaggl <robert@nomatic.org>
2003-12-09 19:56:09 +0000

    added mailto_action (contributed by kris)

--- code/Membership/actions.js
+++ code/Membership/actions.js
@@ -42,3 +42,32 @@ function delete_action() {
    res.data.body = this.renderSkinAsString("delete", sp);
    this.site.renderSkin("page");
 }
+
+/**
+ * send an e-mail to the user owning this membership
+ */
+function mailto_action() {
+   if (req.data.cancel)
+      res.redirect(this._parent.href());
+   else if (req.data.send) {
+      if (req.data.text) {
+         try {
+            var mailbody = this.renderSkinAsString("mailmessage", {text: req.data.text});
+            res.message = sendMail(session.user.email, 
+                                   this.user.email, 
+                                   getMessage("mail.toUser", root.sys_title),
+                                   mailbody);
+            res.redirect(this._parent.href());
+         } catch (err) {
+            res.message = err.toString();
+         }
+      } else {
+         res.message = new Exception("mailTextMissing");
+      }
+   }
+   
+   res.data.action = this.href(req.action);
+   res.data.title = "Send an e-mail to: " + this.username;
+   res.data.body = this.renderSkinAsString("mailto");
+   this.site.renderSkin("page");
+}
