73c4eb8a681f6f8d11e7b96f45288d2a7685a1c0
Robert Gaggl <robert@nomatic.org>
2003-12-11 20:09:08 +0000

    modified onRequest(): apply HopObject.onRequest() before anything else (since context, skinpath etc. are set in this method)

--- code/SysMgr/securityFunctions.js
+++ code/SysMgr/securityFunctions.js
@@ -3,16 +3,11 @@
  * and checks if user is logged in and is sysadmin
  */
 function onRequest() {
-   autoLogin();
-   checkIfLoggedIn(this.href(req.action));
-
+   HopObject.prototype.onRequest.apply(this);
    if (!session.user || !session.user.sysadmin) {
       res.message = new Exception("userNoSysAdmin");
       res.redirect(root.href());
    }
-   // define res.handlers.layout
-   res.handlers.layout = root.getLayout();
-   res.skinpath = [res.handlers.layout.skins];
    // initialize sysmgr-object in session
    if (!session.data.mgr)
       session.data.mgr = new sysmgr();
