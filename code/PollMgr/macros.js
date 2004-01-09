67b777ae5874c3b2a6cf2ff8ba38c762a1597a5b
Tobi Schäfer <interface@p3k.org>
2004-01-09 19:08:51 +0000

    removed obsolete files

--- code/PollMgr/macros.js
+++
@@ -1,9 +0,0 @@
-/**
- * macro writes list of polls to response-object
- * kept for backwards-compatibility only
- */
-
-function pollList_macro(param) {
-   res.write(res.data.pollList);
-   return;
-}
--- code/PollMgr/renderFunctions.js
+++
@@ -1,9 +0,0 @@
-/**
- * check if a story is online or offline
- * and render the listitem view accordingly
- * @param Object poll object to render
- */
-function renderManagerView(poll) {
-   poll.renderSkin(poll.closed == 1 ? "closed" : "open");
-   return;
-}
