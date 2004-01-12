4d5ea7581ab8534fcf6416758d85939b0ff6967c
Tobi Schäfer <interface@p3k.org>
2004-01-12 14:47:25 +0000

    * removed deprecated wrapper macros for output of res.data.xxxlist * replaced call for deprecated macro with response.xxxlist

--- code/Site/macros.js
+++ code/Site/macros.js
@@ -221,16 +221,6 @@ function navigation_macro(param) {
 
 
 /**
- * macro writes storylist to response-object
- * kept for backwards-compatibility only
- */
-function storylist_macro(param) {
-   res.write(res.data.storylist);
-   return;
-}
-
-
-/**
  * macro renders a calendar
  * version 2
  */
