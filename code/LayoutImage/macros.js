1f3222cb0ea85574ae04ddcbd57e7e52ef37cda9
Robert Gaggl <robert@nomatic.org>
2004-03-15 07:37:49 +0000

    added replacelink_macro() contributed my matthias platzer

--- code/LayoutImage/macros.js
+++ code/LayoutImage/macros.js
@@ -22,3 +22,29 @@ function deletelink_macro(param) {
       image.prototype.deletelink_macro.apply(this, [param]);
    return;
 }
+
+/**
+ * render a link to delete action
+ * calls image.deletelink_macro, but only
+ * if the layout in path is the one this image
+ * belongs to
+ */
+function replacelink_macro(param) {
+   if (path.layout != this.layout) {
+      if (session.user) {
+         try {
+            path.layout.images.checkAdd(session.user, req.data.memberlevel);
+         } catch (deny) {
+            return;
+         }
+         Html.openLink({href: path.layout.images.href("create") + "?alias=" + this.alias});
+         if (param.image && this.site.images.get(param.image))
+            this.site.renderImage(this.site.images.get(param.image), param);
+         else
+            res.write(param.text ? param.text : getMessage("manage.replace"));
+         Html.closeLink();
+      }
+      return;
+   }
+   return;
+}
