82aa125ccb28ab30cd48bbd4235d4589ab17479a
Christoph Lincke <kris@diszko.org>
2005-10-04 16:09:46 +0000

    removed gallery support and image macros

--- code/Topic/macros.js
+++ code/Topic/macros.js
@@ -18,24 +18,3 @@ function addstory_macro (param) {
    Html.closeTag("a");
    return;
 }
-
-
-/**
- * Display a link to let the user add a new Image to this topic
- */
-function addimage_macro (param) {
-   try {
-      path.Site.images.checkAdd(session.user, req.data.memberlevel);
-   } catch (deny) {
-      return;
-   }
-   param.linkto = "create";
-   param.urlparam = "topic=" + this.groupname;
-   Html.openTag("a", path.Site.images.createLinkParam(param));
-   if (param.text)
-      res.format(param.text);
-   else
-      res.write(getMessage("ImageTopicMgr.addImageToGallery"));
-   Html.closeTag("a");
-   return;
-}
