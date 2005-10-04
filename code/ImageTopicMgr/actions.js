17e2d1ca671731090697fd1fb723133c3e8805cc
Christoph Lincke <kris@diszko.org>
2005-10-04 13:42:15 +0000

    removed  image topics

--- code/ImageTopicMgr/actions.js
+++
@@ -1,9 +0,0 @@
-/**
- * main action
- */
-function main_action() {
-   res.data.title = getMessage("ImageTopicMgr.mainTitle", {siteTitle: this._parent._parent.title});
-   res.data.body = this.renderSkinAsString ("main");
-   res.handlers.site.renderSkin("page");
-   return;
-}
