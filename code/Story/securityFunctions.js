d42c9f5a7504cf71d25a0eb09a9838dbff31b89c
Christoph Lincke <kris@diszko.org>
2005-10-04 15:46:54 +0000

    removed  gallery macro

--- code/Story/securityFunctions.js
+++ code/Story/securityFunctions.js
@@ -127,7 +127,6 @@ function allowTextMacros(s) {
    s.allowMacro("this.topic");
    s.allowMacro("story.topic");
    s.allowMacro("imageoftheday");
-   s.allowMacro("gallery");
    s.allowMacro("spacer");
 
    // allow module text macros
