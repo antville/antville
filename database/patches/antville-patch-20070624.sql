504675b06af84b343cb32b6bf74a0262da31a36f
Tobi Schäfer <interface@p3k.org>
2007-06-30 18:19:24 +0000

    * Added latest patch  * Renamed patches consistently

---
+++ database/patches/antville-patch-20070624.sql
@@ -0,0 +1,11 @@
+update AV_IMAGE add column IMAGE_PARENT_PROTOTYPE mediumint(10) default NULL;
+
+update AV_IMAGE set IMAGE_PARENT_PROTOTYPE = 'Site' where IMAGE_PROTOTYPE = 'Image';
+update AV_IMAGE set IMAGE_PARENT_PROTOTYPE = 'Layout' where IMAGE_PROTOTYPE = 'LayoutImage';
+update AV_IMAGE set IMAGE_PROTOTYPE = "Image" where IMAGE_PROTOTYPE = "LayoutImage"
+
+update AV_IMAGE add column IMAGE_PARENT varchar(20) default NULL:
+
+update AV_IMAGE set IMAGE_PARENT = IMAGE_F_IMAGE_PARENT, IMAGE_PARENT_PROTOTYPE = "Image" where IMAGE_F_IMAGE_PARENT is not null;
+update AV_IMAGE set IMAGE_PARENT = IMAGE_F_SITE where IMAGE_F_SITE is not null and IMAGE_PARENT is null;
+update AV_IMAGE set IMAGE_PARENT = IMAGE_F_LAYOUT where IMAGE_F_LAYOUT is not null and IMAGE_PARENT is null;
