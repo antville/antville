6750cb8984ae3c3bfac9d3db8ca64d53176e41cb
Tobi Schäfer <interface@p3k.org>
2008-12-11 17:36:42 +0000

    Added patch to fix <% if <% comment.created %> %> macros

---
+++ updater/patch-20081211.js
@@ -0,0 +1,50 @@
+//
+// The Antville Project
+// http://code.google.com/p/antville
+//
+// Copyright 2001-2007 by The Antville People
+//
+// Licensed under the Apache License, Version 2.0 (the ``License'');
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an ``AS IS'' BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// $Revision$
+// $LastChangedBy$
+// $LastChangedDate$
+// $URL$
+//
+
+// Needs to be applied from within antville code (does not work inside updater)
+
+global["patch-20081211"] = function() {
+   root.forEach(function() {
+      var site = root;
+      if (site.layout) {
+         res.handlers.layout = site.layout;
+         res.skinpath = site.layout.getSkinPath();
+         
+         // Fixing the <% if <% comment.created %> %> macro
+         var skin = site.layout.skins.getSkin("Comment", "edit");
+         var source = skin.getSource();
+         res.debug(123)
+         res.debug(source.contains("<% if <% comment.created %>"))
+         var re = /<%(\s+if\s+<%\s+comment\.creat)ed/g;
+         var newSource = source.replace(re, function() {
+            var replacement = arguments[1] + "or";
+            app.log(arguments[0] + " ==> " + replacement);
+            return replacement;
+         });
+         if (newSource !== source) {
+            skin.setSource(newSource);
+         }
+      }
+   });
+}
