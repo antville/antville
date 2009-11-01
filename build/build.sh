8d32022257133af5ee2d5597c792ad62880fe560
Tobi Schäfer <interface@p3k.org>
2009-11-01 10:49:31 +0000

    * Rebooted build environment for Antville and AntClick packages  * Updated helma.jar and launcher.jar  * Added h2.jar as AntClick database (experimental)  * Added build targets for extracting gettext call strings and building JavaScript message files  * Added repository for AntClick containing adaptations necessary for H2 database  * Renamed shell scripts for AntClick from antclick.* to start.*  * Added default login for manage console in server.properties  * Removed obsolete files

--- build/build.sh
+++ build/build.sh
@@ -1,27 +1,57 @@
 #!/bin/sh
 
-## uncomment the following line to set JAVA_HOME
-#export JAVA_HOME=/usr/lib/j2sdk1.4.0
+##
+## The Antville Project
+## http://code.google.com/p/antville
+##
+## Copyright 2001-2007 by The Antville People
+##
+## Licensed under the Apache License, Version 2.0 (the ``License'');
+## you may not use this file except in compliance with the License.
+## You may obtain a copy of the License at
+##
+##    http://www.apache.org/licenses/LICENSE-2.0
+##
+## Unless required by applicable law or agreed to in writing, software
+## distributed under the License is distributed on an ``AS IS'' BASIS,
+## WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+## See the License for the specific language governing permissions and
+## limitations under the License.
+##
+## $Revision:3333 $
+## $LastChangedBy:piefke3000 $
+## $LastChangedDate:2007-09-15 01:25:23 +0200 (Sat, 15 Sep 2007) $
+## $URL$
+##
 
 ##--------------------------------------------
 ## No need to edit anything past here
 ##--------------------------------------------
 
-## get update of necessary ant classes from CVS:
-CVS_ACCESS=:pserver:anonymous@194.232.104.95:/opt/cvs/apps
-cvs -d ${CVS_ACCESS} co -d lib antville/build/lib
-cvs -d ${CVS_ACCESS} co -d extra antville/build/extra
-
-if test -z "${JAVA_HOME}" ; then
-    echo "ERROR: JAVA_HOME not found in your environment."
-    echo "Please, set the JAVA_HOME variable in your environment to match the"
-    echo "location of the Java Virtual Machine you want to use."
+if test -z "${ANT_HOME}" ; then
+    echo "ERROR: ANT_HOME not found in your environment."
+    echo "Please, set the ANT_HOME variable in your environment to match the"
+    echo "location of the Apache Ant installation you want to use."
     exit
 fi
 
-BUILDFILE=build.xml
+BUILD_HOME=$(dirname $0)
+if [ "${BUILD_HOME:0:1}" != "/" ] ; then
+    # convert to absolute path
+    BUILD_HOME=${PWD}/${BUILD_HOME}
+fi
+export BUILD_HOME
 
-CP="${CLASSPATH}:lib/ant.jar:lib/ant-launcher.jar:lib/jsch.jar:lib/ant-jsch.jar"
+if [ -z "$HELMA_HOME" ] ; then
+    HELMA_HOME=$BUILD_HOME/../../../
+fi
 
-${JAVA_HOME}/bin/java -cp ${CP} org.apache.tools.ant.launch.Launcher -buildfile ${BUILDFILE} ${1}
+while [ $# -ne 0  ]
+do
+    ANT_CMD_LINE_ARGS="${ANT_CMD_LINE_ARGS} $1"
+    shift
+done
 
+${ANT_HOME}/bin/ant -Dhelma.dir="${HELMA_HOME}" -Dbuild.dir="${BUILD_HOME}" -Dbase.dir="${PWD}" -lib "${BUILD_HOME}/lib:${HELMA_HOME}/lib" -propertyfile "${BUILD_HOME}/build.properties" -file "${BUILD_HOME}/build.xml" ${ANT_CMD_LINE_ARGS}
+
+exit
