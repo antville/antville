7e123c46462ef2c0671f7e5e120cfd46d58060de
Tobi Schäfer <interface@p3k.org>
2011-02-20 10:03:59 +0000

    Refactored patch to also work reliably with large amounts of data

--- extra/updater/patch-20110210.js
+++ extra/updater/patch-20110210.js
@@ -1,8 +1,10 @@
-//
 // The Antville Project
 // http://code.google.com/p/antville
 //
-// Copyright 2001-2011 by The Antville People
+// Copyright 2007-2011 by Tobi Schäfer.
+//
+// Copyright 2001–2007 Robert Gaggl, Hannes Wallnöfer, Tobi Schäfer,
+// Matthias & Michael Platzer, Christoph Lincke.
 //
 // Licensed under the Apache License, Version 2.0 (the ``License'');
 // you may not use this file except in compliance with the License.
@@ -20,7 +22,6 @@
 // $LastChangedBy$
 // $LastChangedDate$
 // $URL$
-//
 
 // Apply with enabled updater repository via ant patch -Dpatch.id=20110210
 
@@ -28,23 +29,45 @@ if (Root.VERSION.minor < 1.3) {
    throw Error("Please update the Antville codebase to version 1.3 first.");
 }
 
+app.addRepository("modules/helma/Database.js");
+
 var sql = new Sql;
 
 function convertMetadata(prototype, table) {
    table || (table = prototype.name.toLowerCase());
-   sql.retrieve("select id, metadata from $0", table);
+
+   var start = 0;
+   var max, end;
+
+   sql.retrieve("select max(id) as count from $0", table);
    sql.traverse(function() {
-      var newMetadata;
-      var metadata = eval(this.metadata);
-      for (let key in metadata) {
-         let value = metadata[key];
-         if (value === null || value === undefined) {
-            continue;
-         }
-         let parent = prototype.getById(this.id);
-         parent.setMetadata(key, value);
-      }
+      max = this.count;
    });
+
+   while (start < max) {
+      end = start + 1000;
+      sql.retrieve("select id, metadata from $0 where id >= $1 and id < $2", table, start, end);
+      sql.traverse(function() {
+         try {
+            var newMetadata;
+            var parent = prototype.getById(this.id);
+            var metadata = eval(this.metadata);
+            for (let name in metadata) {
+               let value = metadata[name];
+               parent.setMetadata(name, value);
+            }
+         } catch (ex) {
+            app.log("***** Original or converted metadata might not be kosher for " +
+                  prototype.name + " #" + this.id);
+            app.log(ex.rhinoException);
+         }
+      });
+      res.commit();
+      app.log(java.lang.String.format("Processed %d of %d %s objects",
+            java.lang.Integer(start), java.lang.Integer(max), prototype.name));
+      start = end;
+   }
+
    return;
 }
 
