322365f59ba47b8f74c7b5acdedf0b2a9de12cf8
Tobi Schäfer <interface@p3k.org>
2011-02-23 14:42:41 +0000

    Added Root.static_macro() method for easier construction of static URLs via macro

--- code/Root/Root.js
+++ code/Root/Root.js
@@ -1,8 +1,10 @@
-//
 // The Antville Project
 // http://code.google.com/p/antville
 //
-// Copyright 2001-2007 by The Antville People
+// Copyright 2007-2011 by Tobi Schäfer.
+//
+// Copyright 2001–2007 Robert Gaggl, Hannes Wallnöfer, Tobi Schäfer,
+// Matthias & Michael Platzer, Christoph Lincke.
 //
 // Licensed under the Apache License, Version 2.0 (the ``License'');
 // you may not use this file except in compliance with the License.
@@ -20,7 +22,6 @@
 // $LastChangedBy$
 // $LastChangedDate$
 // $URL$
-//
 
 /**
  * @fileOverview Defines the Root prototype.
@@ -406,4 +407,8 @@ Root.prototype.getCreationPermission = function() {
       }
    }
    return true;
+}
+
+Root.prototype.static_macro = function(param, name) {
+   return this.getStaticUrl(name);
 }
