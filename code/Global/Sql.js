a72fd7e1ee8ffe8f85c4267ee1ab9033b411d18b
Tobi Schäfer <interface@p3k.org>
2011-06-06 14:31:23 +0000

    Fixed Sql.SEARCH query to only output distinct IDs to prevent redundant results

--- code/Global/Sql.js
+++ code/Global/Sql.js
@@ -172,7 +172,7 @@ Sql.PURGEREFERRERS = "delete from log where action = 'main' and " +
 Sql.SEARCH = "select content.id, metadata.value from content, site, metadata where site.id = $0 and " +
       "site.id = content.site_id and content.status in ('public', 'shared', 'open') and " +
       "content.id = metadata.parent_id and metadata.name in ('title', 'text') and " +
-      "lower(metadata.value) like lower('%$1%') order by content.created desc limit $2";
+      "lower(metadata.value) like lower('%$1%') group by content.id order by content.created desc limit $2";
 
 /** @constant */
 Sql.MEMBERSEARCH = "select name from account where name $0 '$1' " +
