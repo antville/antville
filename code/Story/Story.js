01ec0ba1e42f1707211f1abd25a1d3981e3f7005
Tobi Schäfer <interface@p3k.org>
2015-02-22 15:07:46 +0100

    Changed order of filters to be applied to story text to make autolinking more reliable

--- code/Story/Story.js
+++ code/Story/Story.js
@@ -592,10 +592,11 @@ Story.prototype.format_filter = function(value, param, mode) {
       break;
 
       default:
+      value = this.macro_filter(format(value), param);
+      value = this.url_filter(value, param);
       var parts = value.split(/(?:\n\n|\r\r|\r\n\r\n)/);
       value = '<p>' + parts.join('</p><p>') + '</p>';
-      value = this.macro_filter(format(value), param);
-      return this.url_filter(value, param);
+      return value;
     }
   }
   return String.EMTPY;
