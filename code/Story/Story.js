8f0a1dcfff5e7a515e0999eb4e776118b95aa4da
Tobi Schäfer <interface@p3k.org>
2015-04-12 15:36:49 +0200

    Fixed bug causing scrambled rendering of Markdown links [foobar](http://…)

--- code/Story/Story.js
+++ code/Story/Story.js
@@ -721,7 +721,7 @@ Story.prototype.macro_filter = function(value) {
  */
 Story.prototype.url_filter = function(value, param, mode) {
   param.limit || (param.limit = 50);
-  var re = /(^|\/>|[(\[\s])(!?(?:https?|ftp):\/\/\S+?)([.,;:)\]"'!?]?)(?=[\s<]|$)/gim;
+  var re = /(^|\/>|[^\]]\(|[\[\s])(!?(?:https?|ftp):\/\/\S+?)([.,;:)\]"'!?]?)(?=[\s<]|$)/gim;
   return value.replace(re, function(str, head, url, tail) {
     if (url.startsWith('!')) {
       return head + url.substring(1) + tail;
