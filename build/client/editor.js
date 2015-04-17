40519bc0ea9975b0f501a680371f95c57039867e
Tobi Schäfer <interface@p3k.org>
2015-04-17 13:26:38 +0200

    Cleaned up all and upgraded some NPM modules

--- build/client/editor.js
+++ build/client/editor.js
@@ -1,11 +1,10 @@
 window.CodeMirror = require('codemirror/lib/codemirror');
 require('codemirror/mode/css/css');
-require('codemirror/mode/javascript/javascript');
-require('codemirror/mode/xml/xml');
+require('codemirror/mode/gfm/gfm');
 require('codemirror/mode/htmlmixed/htmlmixed');
+require('codemirror/mode/javascript/javascript');
 require('codemirror/mode/markdown/markdown');
 require('codemirror/mode/xml/xml');
-require('codemirror/mode/gfm/gfm');
 require('./cm-skin-mode');
 
 window.marked = require('marked/lib/marked');
