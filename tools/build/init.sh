4ee6d89bf520bc0b39cab9d06e3768b228626220
Tobi Schäfer <interface@p3k.org>
2020-03-29 21:31:16 +0200

    chg: moved parser files into tools dir

--- tools/build/init.sh
+++ tools/build/init.sh
@@ -2,11 +2,10 @@
 
 base_dir=$(pwd)
 build_dir=$base_dir/build
-work_dir=$build_dir/work
 
-dist_dir=$work_dir/dist
-export_dir=$work_dir/export
-source_dir=$work_dir/src
+dist_dir=$build_dir/dist
+export_dir=$build_dir/export
+source_dir=$build_dir/src
 
 jar_repo=https://repo1.maven.org/maven2
 antville_repo=https://github.com/antville/antville.git
