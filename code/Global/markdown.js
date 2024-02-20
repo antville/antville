c0cf251e97774dd15de3e814c15d8744673dc115
Tobi Schäfer <interface@p3k.org>
2024-02-20 21:01:07 +0100

    Add work-around for issue with CommonMark erroneously encoding HTML entities
    See <https://github.com/commonmark/commonmark-java/issues/307>

--- code/Global/markdown.js
+++ code/Global/markdown.js
@@ -41,8 +41,15 @@ var renderMarkdown = (function() {
     .extensions(extensions)
     .build();
 
+  // Save HTML entities like `&#150;` from being erroneously encoded
+  // See <https://github.com/commonmark/commonmark-java/issues/307>
+  const entityRegex = /&(#\d+;)/g;
+  const entityMarker = "<ampersand/>";
+  const entityMarkerRegex = new RegExp(entityMarker + entityRegex.source.substring(1), 'g');
+
   return function(str) {
+    str = str.replace(entityRegex, entityMarker + '$1');
     const document = parser.parse(str);
-    return renderer.render(document);
+    return renderer.render(document).replace(entityMarkerRegex, '&$1');
   };
 })();
